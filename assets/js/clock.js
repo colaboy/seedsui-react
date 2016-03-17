e7cb5f17f6908a573835151bf3dbcbd2c16948b7
robort007 <49201650@qq.com>
2016-03-17 11:13:45 +0800

    增强clock

--- assets/js/clock.js
+++ assets/js/clock.js
@@ -6,7 +6,11 @@
         var defaults={
             "time":"00:00",
             "hourClass":"hour",
-            "minuteClass":"minute"
+            "minuteClass":"minute",
+            /*
+            "duration":"500",
+            "delay":"0"
+            */
         }
         params=params||{};
         for(var def in defaults){
@@ -37,6 +41,8 @@
             var minuteDeg=s.getMinuteDeg(hourMinute[1]);
             s.hour.style.WebkitTransform="rotate("+hourDeg+"deg)";
             s.minute.style.WebkitTransform="rotate("+minuteDeg+"deg)";
+            if(!isNaN(s.params.duration))s.container.style.WebkitTransitionDuration=s.params.duration+"ms";
+            if(!isNaN(s.params.delay))s.container.style.WebkitTransitionDelay=s.params.delay+"ms";
         }
         s.getHourDeg=function(hour){
             return hour*30;
@@ -55,13 +61,22 @@
         //Return Clock instance
         return s;
     }
-    window.DataClock=function(){
+    window.DataClock=function(params){
+        /*===============
+        Model
+        ================*/
+        //Containers
         var clocks=document.querySelectorAll("[data-clock]");
         clocks.containers=[];
+        //Params
+        var jsonParams={};
+        if(params)jsonParams=params;
+        /*==============
+        Entry
+        ================*/
         for(var i=0,clock;clock=clocks[i++];){
-            clocks.containers[i]=new Clock(clock,{
-                "time":clock.getAttribute("data-clock")
-            })
+            jsonParams.time=clock.getAttribute("data-clock");
+            clocks.containers[i]=new Clock(clock,jsonParams);
         }
         return clocks;
     }
