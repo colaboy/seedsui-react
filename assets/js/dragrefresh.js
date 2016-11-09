613a559b5bf037628530c61e036debf4ae26112c
colaboy <49201650@qq.com>
2016-11-09 15:45:30 +0800

    修改dragrefresh控件

--- assets/js/dragrefresh.js
+++ assets/js/dragrefresh.js
@@ -1,21 +1,23 @@
 //Dragrefresh 下拉刷新
 (function(window,document,undefined){
-	window.Dragrefresh=function(container,params){
+	window.Dragrefresh=function(params){
 		/*==================
 		  Model
 		  ==================*/
 		var defaults={
-			"isDisableTop":false,
-			"isDisableBottom":false,
-			"minScrollTop":0,
-			"threshold":100,
-			"thresholdMaxRange":100,
-			"refreshHideTop":0,
-			"duration":150,
-			"timeout":5000,
+			parent:"article",
+			isDisableTop:false,
+			isDisableBottom:false,
+			minScrollTop:0,
+			threshold:100,
+			thresholdMaxRange:100,
+			refreshHideTop:0,
+			duration:150,
+			timeout:5000,
 
-			"bottomContainerClass":"loading-more",
-			"bottomContainerLoadingClass":"loading",
+			topContainerClass:"dragrefresh",
+			bottomContainerClass:"loading-more",
+			bottomContainerLoadingClass:"loading",
 
 			/*callbacks
 			onRefreshStart:function(Dragrefresh)
@@ -36,29 +38,29 @@
 		//最大拉动值
 		s.params.thresholdMax=s.params.threshold+s.params.thresholdMaxRange;
 		//Container
-		s.container=typeof container=="string"?document.querySelector(container):container;
+		s.parent=typeof s.params.parent=="string"?document.querySelector(s.params.parent):s.params.parent;
 		//创建DOM
 		s.createRefresh=function(){
 			if(s.topContainer)return;
 			s.topContainer=document.createElement("div");
-			s.topContainer.setAttribute("class","dragrefresh box box-middlecenter");
+			s.topContainer.setAttribute("class",s.params.topContainerClass);
 			var iconSvg='<svg width="1000.6px" height="1000.6px" viewBox="0 0 1000.6 1000.6" xml:space="preserve">'+
 						'<path d="M867.4,456.1c-24.1,0-43.8,19.7-43.8,43.8c0,1.5,0.1,3.1,0.3,4.6c-2.2,176.4-147.1,319.6-323.7,319.6 c-178.5,0-323.8-145.3-323.8-323.8s145.3-323.8,323.8-323.8c62.8,0,122.8,17.7,174.4,50.8l-29,52.2c0,0,138.4,2.2,149.2,2.4 c10.8,0.2,14.6-5.6,14.6-5.6s5.1-5.8,2.4-15.5c-2.6-9.7-43.2-162.2-43.2-162.2l-38.5,61.1c-67.3-45.7-146.7-70.1-229.8-70.1 c-226.6,0-411,184.4-411,411s184.4,411,411,411c225.8,0,410.1-183.7,410.9-407.3l0.2-4.2C911.2,475.7,891.6,456.1,867.4,456.1z"/>'+
 						'</svg>';
 			s.topContainer.innerHTML=iconSvg;
-			s.container.appendChild(s.topContainer);
+			s.parent.parentNode.appendChild(s.topContainer);
 		};
 		s.createRefresh();
 		s.bottomContainer=null;
 		s.createBottomContainer=function(){
-			s.bottomContainer=s.container.querySelector("."+s.params.bottomContainerClass);
+			s.bottomContainer=s.parent.querySelector("."+s.params.bottomContainerClass);
 			if(!s.bottomContainer){
 				s.bottomContainer=document.createElement("div");
 				s.bottomContainer.setAttribute("class",s.params.bottomContainerClass);
 				var spinnerdiv=document.createElement("div");
 				spinnerdiv.setAttribute("class",s.params.bottomContainerLoadingClass);
 				s.bottomContainer.appendChild(spinnerdiv);
-				s.container.appendChild(s.bottomContainer);
+				s.parent.appendChild(s.bottomContainer);
 			}
 		}
 		if(s.params.onBottom)s.createBottomContainer();
@@ -66,25 +68,8 @@
 		/*==================
 		  Mothod
 		  ==================*/
-		//添加动画
-		/*s.addTransition=function(duration){
-			if(!duration)duration=s.params.duration;
-			s.topContainer.style.webkitTransitionDuration=duration+"ms";
-		};*/
-		//移除动画
-		/*s.removeTransition=function(){
-			s.topContainer.style.webkitTransitionDuration="0ms";
-		};*/
-		//变形
-		/*s.transform=function(y,deg){
-			if(!y)y=s.touches.posY;
-			if(!deg)deg=s.touches.rotateDeg;
-			s.topContainer.style.webkitTransform='translate3d(0,' + y + 'px,0) rotate(' + deg + 'deg)';
-		}*/
 		//旋转,10W毫秒，旋转4万6千度
 		s.spinner=function(){
-			/*s.addTransition("100000");
-			s.transform(null,"46000");*/
 			s.topContainer.style.webkitTransitionDuration="100000ms";
 			s.topContainer.style.webkitTransform='translate3d(0,' + s.touches.posY + 'px,0) rotate(46000deg)';
 		}
@@ -95,8 +80,6 @@
 			},100);
 		}
 		s.cancelSpinner=function(){
-			/*s.removeTransition();
-			s.transform(null,"0");*/
 			s.topContainer.style.webkitTransitionDuration="0ms";
 			s.topContainer.style.webkitTransform='translate3d(0,' + s.touches.posY + 'px,0) rotate(0deg)';
 		};
@@ -106,10 +89,8 @@
 			//停止旋转
 			s.cancelSpinner();
 			//收起
-			//s.addTransition();
 			s.topContainer.style.webkitTransitionDuration=s.params.duration+"ms";
 			s.touches.posY=s.params.refreshHideTop;
-			//s.transform(s.touches.posY,s.touches.rotateDeg);
 			s.topContainer.style.webkitTransform='translate3d(0,' + s.touches.posY + 'px,0) rotate(' + s.touches.rotateDeg + 'deg)';
 
 			s.isHid=true;
@@ -118,21 +99,19 @@
 		s.show=function(){
 			s.isHid=false;
 			//收到指定位置
-			//s.addTransition();
 			s.topContainer.style.webkitTransitionDuration=s.params.duration+"ms";
 			if(s.touches.posY==s.params.threshold){//不执行onTransitionEnd的情况，直接旋转
 				s.delaySpinner();
 			}
 			s.touches.posY=s.params.threshold;
-			//s.transform(s.touches.posY);
 			s.topContainer.style.webkitTransform='translate3d(0,' + s.touches.posY + 'px,0) rotate(' + s.touches.rotateDeg + 'deg)';
 		}
 		//销毁对象
 		s.destroyTop=function(){
-			s.container.removeChild(s.topContainer);
+			s.parent.parentNode.removeChild(s.topContainer);
 		}
 		s.destroyBottom=function(){
-			s.container.removeChild(s.bottomContainer);
+			s.parent.removeChild(s.bottomContainer);
 		}
 		s.destroy=function(){
 			s.destroyTop();
@@ -178,7 +157,7 @@
 		s.events=function(detach){
 			var action=detach?"removeEventListener":"addEventListener";
 			if(s.params.isDisableTop===false){
-				var touchTarget=s.container;
+				var touchTarget=s.parent;
 				touchTarget[action]("touchstart",s.onTouchStart,false);
 				touchTarget[action]("touchmove",s.onTouchMove,false);
 				touchTarget[action]("touchend",s.onTouchEnd,false);
@@ -188,7 +167,7 @@
 			}
 			if(s.params.isDisableBottom===false){
 				//绑定底部事件
-				if(s.bottomContainer)s.container[action]("scroll",s.onScroll,false);
+				if(s.bottomContainer)s.parent[action]("scroll",s.onScroll,false);
 			}
 		}
 		//attach、detach事件
@@ -221,9 +200,9 @@
 		s.onTouchStart=function(e){
 			if(s.isRefreshEnd===false)return;
 
-			s.container.addEventListener("touchmove",s.preventDefault,false);
+			s.parent.addEventListener("touchmove",s.preventDefault,false);
 			//如果不在顶部，则不触发
-			if(s.container.scrollTop>s.params.minScrollTop)s.touches.isTop=false;
+			if(s.parent.scrollTop>s.params.minScrollTop)s.touches.isTop=false;
 			else s.touches.isTop=true;
 
 			//s.removeTransition();
@@ -251,7 +230,7 @@
 			}
 			
 			if(s.touches.vertical==1 || !s.touches.isTop){//向上滑动或者不在顶部
-				s.container.removeEventListener("touchmove",s.preventDefault,false);	
+				s.parent.removeEventListener("touchmove",s.preventDefault,false);	
 			}else if(s.touches.vertical===-1){//下拉
 				s.touches.posY=s.params.refreshHideTop+s.touches.diffY;
 				if(s.touches.posY<s.params.thresholdMax){
@@ -268,7 +247,7 @@
 
 			if(s.isRefreshEnd===false)return;
 
-			s.container.removeEventListener("touchmove",s.preventDefault,false);
+			s.parent.removeEventListener("touchmove",s.preventDefault,false);
 			if(s.touches.posY!=0){//下拉情况下
 				if(s.touches.posY<s.params.threshold){//如果小于hold值，则收起刷新
 					s.hide();
