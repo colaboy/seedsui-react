90cf3f5bab10c8d358ea40cbfde5570d3d41eddc
colaboy <49201650@qq.com>
2017-08-07 08:52:35 +0800

    scrollpicker修改

--- src/assets/scripts/dialog.js
+++ src/assets/scripts/dialog.js
@@ -1,216 +1,219 @@
 // Dialog 自定义弹出框
-(function(window, document, undefined) {
-  window.Dialog = function(params) {
-    /* =========================
-    Model
-    =========================== */
-    var defaults = {
-      overflowContainer: document.body,
-      overflowContainerActiveClass: 'overflow-hidden',
+(function (window, document, undefined) {
+window.Dialog = function (params) {
+  /* -------------------------
+  Model
+  ------------------------- */
+  var defaults = {
+    overflowContainer: document.body,
+    overflowContainerActiveClass: 'overflow-hidden',
 
-      position: 'middle',
+    position: 'middle',
 
-      animationAttr: 'data-animation',
-      animation: 'fade',
+    animationAttr: 'data-animation',
+    animation: 'fade',
 
-      mask: null,
-      wrapper: null,
+    mask: null,
+    wrapper: null,
 
-      maskClass: 'mask',
-      maskActiveClass: 'active',
-      maskFeatureClass: 'dialog-mask',
+    maskClass: 'mask',
+    maskActiveClass: 'active',
+    maskFeatureClass: 'dialog-mask',
 
-      dialogClass: 'dialog',
-      dialogActiveClass: 'active',
+    dialogClass: 'dialog',
+    dialogActiveClass: 'active',
 
-      css: {},
-      maskCss: {},
-      isClickMaskHide: true
-      /* callbacks
-      onClick:function(Dialog)
-      onClickMask:function(Dialog)
-      onTransitionEnd:function(Dialog)
-      onShowed(Dialog)// 显示动画结束后回调
-      onHid(Dialog)// 隐藏动画结束后回调
-       */
-    }
-    params = params || {}
-    for (var def in defaults) {
-      if (params[def] === undefined) {
-        params[def] = defaults[def]
-      }
-    }
-    // Dialog
-    var s = this
-
-    // Params
-    s.params = params
-    // Mask
-    s.mask
-    // Dialog(外层生成的包裹容器)
-    s.dialog
-    // Wrapper(源容器)
-    s.wrapper
-    // Parent(父容器，为了方便在源容器处插入包裹容器)
-    s.parent
-    // OverflowContainer
-    s.overflowContainer = typeof s.params.overflowContainer == 'string' ? document.querySelector(s.params.overflowContainer) : s.params.overflowContainer
-
-    // Mask
-    s.createMask = function() {
-      var mask = document.createElement('div')
-      mask.setAttribute('class', s.params.maskClass + ' ' + s.params.maskFeatureClass)
-      return mask
-    }
-    // Dialog
-    s.createDialog = function() {
-      var dialog = document.createElement('div')
-      dialog.classList.add(s.params.dialogClass, s.params.position)
-      dialog.setAttribute(s.params.animationAttr, s.params.animation)
-      return dialog
-    }
-    s.create = function() {
-      if (s.params.mask) s.mask = typeof s.params.mask === 'string' ? document.querySelector(s.params.mask) : s.params.mask
-      if (s.mask) {
-        s.dialog = s.mask.querySelector('.' + s.params.dialogClass)
-        s.wrapper = s.dialog.children[0]
-      } else {
-        s.wrapper = typeof s.params.wrapper == 'string' ? document.querySelector(s.params.wrapper) : s.params.wrapper
-      }
-      if (!s.wrapper) {
-        console.log('SeedsUI Error：未找到Dialog的DOM对象，请检查传入参数是否正确')
-        return
-      }
-      s.parent = s.wrapper.parentNode
-      // 插入Dialog
-      s.dialog = s.createDialog()
-      s.parent.insertBefore(s.dialog, s.wrapper)
-      s.dialog.appendChild(s.wrapper)
-      // 插入遮罩
-      s.mask = s.createMask()
-      s.mask.appendChild(s.dialog)
-      s.parent.appendChild(s.mask)
-    }
-    s.create()
-    s.update = function() {
-      // Dialog Css
-      for (var style in s.params.css) {
-        s.dialog.style[style] = s.params.css[style]
-      }
-      // Mask Css
-      for (var style in s.params.maskCss) {
-        s.mask.style[style] = s.params.maskCss[style]
-      }
-      // 源容器显示
-      s.wrapper.style.display = 'block'
-    }
-    s.update()
-
-    /* =========================
-    Method
-    =========================== */
-    
-    s.showMask = function() {
-      s.mask.classList.add(s.params.maskActiveClass)
-    }
-    s.hideMask = function() {
-      s.mask.classList.remove(s.params.maskActiveClass)
-    }
-    s.destroyMask = function() {
-      s.parent.removeChild(s.mask)
-    }
-    s.showDialog = function() {
-      s.dialog.classList.add(s.params.dialogActiveClass)
-    }
-    s.hideDialog = function() {
-      s.dialog.classList.remove(s.params.dialogActiveClass)
-    }
-    s.destroyDialog = function() {
-      s.parent.removeChild(s.dialog)
-    }
-    s.isHid = true
-    s.hide = function() {
-      s.isHid = true
-      s.hideMask()
-      s.hideDialog()
-      // 显示滚动条
-      if (s.overflowContainer) s.overflowContainer.classList.remove(s.params.overflowContainerActiveClass)
-    }
-    s.show = function() {
-      s.isHid = false
-      s.showMask()
-      s.showDialog()
-      // 禁用滚动条
-      if (s.overflowContainer) s.overflowContainer.classList.add(s.params.overflowContainerActiveClass)
-    }
-    s.toggle = function() {
-      if (s.isHid) {
-        s.show()
-      } else {
-        s.hide()
-      }
-    }
-    s.destroy = function() {
-      s.destroyMask()
-      //  s.destroyDialog()
-    }
-    // 设置
-    s.setOnClick = function(fn) {
-      s.params.onClick = fn
-    }
-    s.setOnClickMask = function(fn) {
-      s.params.onClickMask = fn
-    }
-
-    /* ================
-    Control
-    ================ */
-
-    s.events = function(detach) {
-      var touchTarget = s.dialog
-      var action = detach ? 'removeEventListener' : 'addEventListener'
-      touchTarget[action]('click', s.onClick, false)
-      touchTarget[action]('webkitTransitionEnd', s.onTransitionEnd, false)
-      // 遮罩
-      s.mask[action]('click', s.onClickMask, false)
-    }
-    // attach、dettach事件
-    s.attach = function(event) {
-      s.events()
-    }
-    s.detach = function(event) {
-      s.events(true)
-    }
-    s.onClick = function(e) {
-      s.target = e.target
-      // CallBack onClick
-      if (s.params.onClick) s.params.onClick(s)
-    }
-    s.onClickMask = function(e) {
-      if (e.target === s.mask) {
-        s.target = e.target
-        // CallBack onClickMask
-        if (s.params.onClickMask) s.params.onClickMask(s)
-        if (s.params.isClickMaskHide) s.hide()
-      }
-    }
-    s.onTransitionEnd = function(e) {
-      if (e.propertyName == 'visibility') return
-      s.target = e.target
-      // Callback onTransitionEnd
-      if (s.params.onTransitionEnd) s.params.onTransitionEnd(s)
-      if (s.isHid) {
-        // Callback onHid
-        if (s.params.onHid) s.params.onHid(s)
-      } else {
-        // Callback onShowed
-        if (s.params.onShowed) s.params.onShowed(s)
-      }
-    }
-
-    s.init = function() {
-      s.attach()
-    }
-    s.init()
+    css: {},
+    maskCss: {},
+    isClickMaskHide: true
+    /* callbacks
+    onClick:function(Dialog)
+    onClickMask:function(Dialog)
+    onTransitionEnd:function(Dialog)
+    onShowed(Dialog)// 显示动画结束后回调
+    onHid(Dialog)// 隐藏动画结束后回调
+      */
   }
+  params = params || {}
+  for (var def in defaults) {
+    if (params[def] === undefined) {
+      params[def] = defaults[def]
+    }
+  }
+
+  // Dialog
+  var s = this
+
+  // Params
+  s.params = params
+  // Mask
+  s.mask
+  // Dialog(外层生成的包裹容器)
+  s.dialog
+  // Wrapper(源容器)
+  s.wrapper
+  // Parent(父容器，为了方便在源容器处插入包裹容器)
+  s.parent
+  // OverflowContainer
+  s.overflowContainer = typeof s.params.overflowContainer === 'string' ? document.querySelector(s.params.overflowContainer) : s.params.overflowContainer
+
+  // Mask
+  s.createMask = function () {
+    var mask = document.createElement('div')
+    mask.setAttribute('class', s.params.maskClass + ' ' + s.params.maskFeatureClass)
+    return mask
+  }
+
+  // Dialog
+  s.createDialog = function () {
+    var dialog = document.createElement('div')
+    dialog.classList.add(s.params.dialogClass, s.params.position)
+    dialog.setAttribute(s.params.animationAttr, s.params.animation)
+    return dialog
+  }
+  s.create = function () {
+    if (s.params.mask) s.mask = typeof s.params.mask === 'string' ? document.querySelector(s.params.mask) : s.params.mask
+    if (s.mask) {
+      s.dialog = s.mask.querySelector('.' + s.params.dialogClass)
+      s.wrapper = s.dialog.children[0]
+      return
+    }
+    s.wrapper = typeof s.params.wrapper === 'string' ? document.querySelector(s.params.wrapper) : s.params.wrapper
+    if (!s.wrapper) {
+      console.log('SeedsUI Error：未找到Dialog的DOM对象，请检查传入参数是否正确')
+      return
+    }
+    s.parent = s.wrapper.parentNode
+    // 插入Dialog
+    s.dialog = s.createDialog()
+    s.parent.insertBefore(s.dialog, s.wrapper)
+    s.dialog.appendChild(s.wrapper)
+    // 插入遮罩
+    s.mask = s.createMask()
+    s.mask.appendChild(s.dialog)
+    s.parent.appendChild(s.mask)
+  }
+  s.create()
+  s.update = function () {
+    var style
+    // Dialog Css
+    for (style in s.params.css) {
+      s.dialog.style[style] = s.params.css[style]
+    }
+    // Mask Css
+    for (style in s.params.maskCss) {
+      s.mask.style[style] = s.params.maskCss[style]
+    }
+    // 源容器显示
+    s.wrapper.style.display = 'block'
+  }
+  s.update()
+
+  /* -------------------------
+  Method
+  ------------------------- */
+  s.showMask = function () {
+    s.mask.classList.add(s.params.maskActiveClass)
+  }
+  s.hideMask = function () {
+    s.mask.classList.remove(s.params.maskActiveClass)
+  }
+  s.destroyMask = function () {
+    s.parent.removeChild(s.mask)
+  }
+  s.showDialog = function () {
+    s.dialog.classList.add(s.params.dialogActiveClass)
+  }
+  s.hideDialog = function () {
+    s.dialog.classList.remove(s.params.dialogActiveClass)
+  }
+  s.destroyDialog = function () {
+    s.parent.removeChild(s.dialog)
+  }
+  s.isHid = true
+  s.hide = function () {
+    s.isHid = true
+    s.hideMask()
+    s.hideDialog()
+    // 显示滚动条
+    if (s.overflowContainer) s.overflowContainer.classList.remove(s.params.overflowContainerActiveClass)
+  }
+  s.show = function () {
+    s.isHid = false
+    s.showMask()
+    s.showDialog()
+    // 禁用滚动条
+    if (s.overflowContainer) s.overflowContainer.classList.add(s.params.overflowContainerActiveClass)
+  }
+  s.toggle = function () {
+    if (s.isHid) {
+      s.show()
+    } else {
+      s.hide()
+    }
+  }
+  s.destroy = function () {
+    s.destroyMask()
+    //  s.destroyDialog()
+  }
+
+  // 设置
+  s.setOnClick = function (fn) {
+    s.params.onClick = fn
+  }
+  s.setOnClickMask = function (fn) {
+    s.params.onClickMask = fn
+  }
+
+  /* -------------------------
+  Control
+  ------------------------- */
+  s.events = function (detach) {
+    var touchTarget = s.dialog
+    var action = detach ? 'removeEventListener' : 'addEventListener'
+    touchTarget[action]('click', s.onClick, false)
+    touchTarget[action]('webkitTransitionEnd', s.onTransitionEnd, false)
+    // 遮罩
+    s.mask[action]('click', s.onClickMask, false)
+  }
+
+  // attach、dettach事件
+  s.attach = function (event) {
+    s.events()
+  }
+  s.detach = function (event) {
+    s.events(true)
+  }
+  s.onClick = function (e) {
+    s.target = e.target
+    // CallBack onClick
+    if (s.params.onClick) s.params.onClick(s)
+  }
+  s.onClickMask = function (e) {
+    if (e.target === s.mask) {
+      s.target = e.target
+      // CallBack onClickMask
+      if (s.params.onClickMask) s.params.onClickMask(s)
+      if (s.params.isClickMaskHide) s.hide()
+    }
+  }
+  s.onTransitionEnd = function (e) {
+    if (e.propertyName === 'visibility') return
+    s.target = e.target
+    // Callback onTransitionEnd
+    if (s.params.onTransitionEnd) s.params.onTransitionEnd(s)
+    if (s.isHid) {
+      // Callback onHid
+      if (s.params.onHid) s.params.onHid(s)
+    } else {
+      // Callback onShowed
+      if (s.params.onShowed) s.params.onShowed(s)
+    }
+  }
+
+  s.init = function () {
+    s.attach()
+  }
+  s.init()
+}
 })(window, document, undefined);
--- src/assets/scripts/scrollpicker.date.js
+++ src/assets/scripts/scrollpicker.date.js
@@ -1,299 +1,302 @@
 //SpDate 扩展scrollpicker日期控件 (require scrollpikcer.js)
-(function(window, document, undefined) {
-window.SpDate = function (params) {
-  // 参数改写
-  var onDateClickDone = params.onClickDone
-  var onDateClickCancel = params.onClickCancel
-  var onDateScrollEnd = params.onScrollEnd
-  var nowDate = new Date()
-  /* ----------------
-    Model
-    ---------------- */
-  var defaults = {
-    viewType: 'date', // 'date','month','time','datetime'
-    isSimpleYear: false,
+(function (window, document, undefined) {
+  window.SpDate = function (params) {
+    // 参数改写
+    var onDateClickDone = params.onClickDone
+    var onDateClickCancel = params.onClickCancel
+    var onDateScrollEnd = params.onScrollEnd
+    params.onClickDone = undefined
+    params.onClickCancel = undefined
+    params.onScrollEnd = undefined
+    var nowDate = new Date()
+    /* ----------------
+      Model
+      ---------------- */
+    var defaults = {
+      viewType: 'date', // 'date','month','time','datetime'
+      isSimpleYear: false,
 
-    yearClass: 'text-center',
-    monthClass: 'text-center',
-    dayClass: 'text-center',
-    hourClass: 'text-center',
-    minuteClass: 'text-center',
+      yearClass: 'text-center',
+      monthClass: 'text-center',
+      dayClass: 'text-center',
+      hourClass: 'text-center',
+      minuteClass: 'text-center',
 
-    yearsData: null,
-    monthsData: null,
-    daysData: null,
-    hoursData: null,
-    minutesData: null,
+      yearsData: null,
+      monthsData: null,
+      daysData: null,
+      hoursData: null,
+      minutesData: null,
 
-    defaultYear: '',
-    defaultMonth: '',
-    defaultDay: '',
-    defaultHour: '',
-    defaultMinute: '',
+      defaultYear: '',
+      defaultMonth: '',
+      defaultDay: '',
+      defaultHour: '',
+      defaultMinute: '',
 
-    disableYears: null,
-    disableMonths: null,
-    disableDays: null,
-    disableHours: null,
-    disableMinutes: null,
-    disableDateTime: null,
+      disableYears: null,
+      disableMonths: null,
+      disableDays: null,
+      disableHours: null,
+      disableMinutes: null,
+      disableDateTime: null,
 
-    minYear: nowDate.getFullYear() - 10, // 1950
-    maxYear: nowDate.getFullYear() + 10, // 2050
+      minYear: nowDate.getFullYear() - 10, // 1950
+      maxYear: nowDate.getFullYear() + 10, // 2050
 
-    yyUnit: '年',
-    MMUnit: '月',
-    ddUnit: '日',
-    hhUnit: '时',
-    mmUnit: '分',
+      yyUnit: '年',
+      MMUnit: '月',
+      ddUnit: '日',
+      hhUnit: '时',
+      mmUnit: '分',
 
-    onClickCancel: function (e) {
-      if (onDateClickCancel) onDateClickCancel(s)
-      else e.hide()
-    },
-    onScrollEnd: function (e) {
-      // 根据月份算日
-      if ((e.params.viewType === 'date' || e.params.viewType === 'datetime') && (e.activeSlot.index === 0 || e.activeSlot.index === 1)) {
-        var year = e.activeOptions[0]['key']
-        var month = e.activeOptions[1]['key']
-        var defaultDay = e.activeOptions[2]['key']
-        updateDays(year, month, defaultDay) // 更新总天数
+      onClickCancel: function (e) {
+        if (onDateClickCancel) onDateClickCancel(s)
+        else e.hide()
+      },
+      onClickDone: function (e) {
+        e.activeText = getActiveText(e.activeOptions)
+        if (onDateClickDone) onDateClickDone(e)
+      },
+      onScrollEnd: function (e) {
+        // 根据月份算日
+        if ((e.params.viewType === 'date' || e.params.viewType === 'datetime') && (e.activeSlot.index === 0 || e.activeSlot.index === 1)) {
+          var year = e.activeOptions[0]['key']
+          var month = e.activeOptions[1]['key']
+          var defaultDay = e.activeOptions[2]['key']
+          updateDays(year, month, defaultDay) // 更新总天数
+        }
+        // 回调
+        if (onDateScrollEnd) onDateScrollEnd(e)
       }
-      // 回调
-      if (onDateScrollEnd) onDateScrollEnd(s)
     }
-  }
-  params = params || {}
-  for (var def in defaults) {
-    if (params[def] === undefined) {
-      params[def] = defaults[def]
+    params = params || {}
+    for (var def in defaults) {
+      if (params[def] === undefined) {
+        params[def] = defaults[def]
+      }
     }
-  }
-  var s = new Scrollpicker(params)
+    var s = new Scrollpicker(params)
 
-  s.params.onClickDone = function (e) {
-    e.activeText = getActiveText(e.activeOptions)
-    if (onDateClickDone) onDateClickDone(e)
-  }
-  function trim (str) {
-    return str.replace(/(^\s*)|(\s*$)/g, '')
-  }
-  // 设置默认值
-  s.setDefaultYear = function (yearKey) {
-    s.params.defaultYear = trim('' + yearKey)
-  }
-  s.setDefaultMonth = function (monthKey) {
-    s.params.defaultMonth = trim('' + monthKey)
-  }
-  s.setDefaultDay = function (dayKey) {
-    s.params.defaultDay = trim('' + dayKey)
-  }
-  s.setDefaultHour = function (hourKey) {
-    s.params.defaultHour = trim('' + hourKey)
-  }
-  s.setDefaultMinute = function (minuteKey) {
-    s.params.defaultMinute = trim('' + minuteKey)
-  }
+    function trim(str) {
+      return str.replace(/(^\s*)|(\s*$)/g, '')
+    }
+    // 设置默认值
+    s.setDefaultYear = function (yearKey) {
+      s.params.defaultYear = trim('' + yearKey)
+    }
+    s.setDefaultMonth = function (monthKey) {
+      s.params.defaultMonth = trim('' + monthKey)
+    }
+    s.setDefaultDay = function (dayKey) {
+      s.params.defaultDay = trim('' + dayKey)
+    }
+    s.setDefaultHour = function (hourKey) {
+      s.params.defaultHour = trim('' + hourKey)
+    }
+    s.setDefaultMinute = function (minuteKey) {
+      s.params.defaultMinute = trim('' + minuteKey)
+    }
 
-  // 默认时间
-  s.updateDetault = function () {
-    nowDate = new Date()
-    // 默认值
-    if (!s.params.defaultYear) {
-      var yyyy = nowDate.getFullYear()
-      var _yyyy = '' + yyyy
-      s.setDefaultYear(_yyyy)
+    // 默认时间
+    s.updateDetault = function () {
+      nowDate = new Date()
+      // 默认值
+      if (!s.params.defaultYear) {
+        var yyyy = nowDate.getFullYear()
+        var _yyyy = '' + yyyy
+        s.setDefaultYear(_yyyy)
+      }
+      if (!s.params.defaultMonth) {
+        var MM = nowDate.getMonth() + 1
+        var _MM = MM.toString().length === 1 ? '0' + MM : MM
+        s.setDefaultMonth(_MM)
+      }
+      if (!s.params.defaultDay) {
+        var dd = nowDate.getDate()
+        var _dd = dd.toString().length === 1 ? '0' + dd : dd
+        s.setDefaultDay(_dd)
+      }
+      if (!s.params.defaultHour) {
+        var hh = nowDate.getHours()
+        var _hh = hh.toString().length === 1 ? '0' + hh : hh
+        s.setDefaultHour(_hh)
+      }
+      if (!s.params.defaultMinute) {
+        var mm = nowDate.getMinutes()
+        var _mm = mm.toString().length === 1 ? '0' + mm : mm
+        s.setDefaultMinute(_mm)
+      }
     }
-    if (!s.params.defaultMonth) {
-      var MM = nowDate.getMonth() + 1
-      var _MM = MM.toString().length === 1 ? '0' + MM : MM
-      s.setDefaultMonth(_MM)
-    }
-    if (!s.params.defaultDay) {
-      var dd = nowDate.getDate()
-      var _dd = dd.toString().length === 1 ? '0' + dd : dd
-      s.setDefaultDay(_dd)
-    }
-    if (!s.params.defaultHour) {
-      var hh = nowDate.getHours()
-      var _hh = hh.toString().length === 1 ? '0' + hh : hh
-      s.setDefaultHour(_hh)
-    }
-    if (!s.params.defaultMinute) {
-      var mm = nowDate.getMinutes()
-      var _mm = mm.toString().length === 1 ? '0' + mm : mm
-      s.setDefaultMinute(_mm)
-    }
-  }
-  s.updateDetault()
+    s.updateDetault()
 
-  // 年
-  s.years = []
-  if (s.params.yearsData) {
-    s.years = s.params.yearsData
-  } else {
-    for (var yyyy = s.params.minYear; yyyy <= s.params.maxYear; yyyy++) {
-      s.years.push({
-        'key': '' + yyyy,
-        'value': s.params.isSimpleYear ? yyyy.toString().substring(2, 4) + s.params.yyUnit : yyyy + s.params.yyUnit,
-        'flag': 'date'
-      })
+    // 年
+    s.years = []
+    if (s.params.yearsData) {
+      s.years = s.params.yearsData
+    } else {
+      for (var yyyy = s.params.minYear; yyyy <= s.params.maxYear; yyyy++) {
+        s.years.push({
+          'key': '' + yyyy,
+          'value': s.params.isSimpleYear ? yyyy.toString().substring(2, 4) + s.params.yyUnit : yyyy + s.params.yyUnit,
+          'flag': 'date'
+        })
+      }
     }
-  }
-  // 月
-  s.months = []
-  if (s.params.monthsData) {
-    s.months = s.params.monthsData
-  } else {
-    for (var MM = 1; MM <= 12; MM++) {
-      var _MM = MM.toString().length === 1 ? '0' + MM : MM
-      s.months.push({
-        'key': '' + _MM,
-        'value': _MM + s.params.MMUnit,
-        'flag': 'date'
-      })
+    // 月
+    s.months = []
+    if (s.params.monthsData) {
+      s.months = s.params.monthsData
+    } else {
+      for (var MM = 1; MM <= 12; MM++) {
+        var _MM = MM.toString().length === 1 ? '0' + MM : MM
+        s.months.push({
+          'key': '' + _MM,
+          'value': _MM + s.params.MMUnit,
+          'flag': 'date'
+        })
+      }
     }
-  }
-  // 日
-  s.days = []
-  var currentMaxday = new Date(nowDate.getFullYear(), nowDate.getMonth() + 1, 0).getDate()
-  if (s.params.daysData) {
-    s.days = s.params.daysData
-  } else {
-    for (var dd = 1; dd <= currentMaxday; dd++) {
-      var _dd = dd.toString().length === 1 ? '0' + dd : dd
-      s.days.push({
-        'key': '' + _dd,
-        'value': _dd + s.params.ddUnit,
-        'flag': 'date'
-      })
-    }
-  }
-
-  function updateDays (year, month, defaultDay) {
-    var maxDay = new Date(year, month, 0).getDate()
+    // 日
     s.days = []
-    for (var dd = 1; dd <= maxDay; dd++) {
-      var _dd = dd.toString().length === 1 ? '0' + dd : dd
-      s.days.push({
-        'key': '' + _dd,
-        'value': _dd + s.params.ddUnit,
-        'flag': 'date'
-      })
-    }
-    var defaultKey = defaultDay
-    if (s.days.length < defaultDay) defaultKey = s.days[s.days.length - 1]['key']
-    s.replaceSlot(2, s.days, defaultKey, s.params.dayClass) // 修改第三项
-  }
-
-  // 时
-  s.hours = []
-  if (s.params.hoursData) {
-    s.hours = s.params.hoursData
-  } else {
-    for (var hh = 0; hh <= 23; hh++) {
-      var _hh = hh.toString().length === 1 ? '0' + hh : hh
-      s.hours.push({
-        'key': '' + _hh,
-        'value': _hh + s.params.hhUnit,
-        'flag': 'time'
-      })
-    }
-  }
-
-  // 分
-  s.minutes = []
-  if (s.params.minutesData) {
-    s.minutes = s.params.minutesData
-  } else {
-    for (var mm = 0; mm <= 59; mm++) {
-      var _mm = mm.toString().length === 1 ? '0' + mm : mm
-      s.minutes.push({
-        'key': '' + _mm,
-        'value': _mm + s.params.mmUnit,
-        'flag': 'time'
-      })
-    }
-  }
-
-  /* ----------------
-  Method
-  ---------------- */
-  function getActiveText (activeData) {
-    var activeText = ''
-    var dateArr = []
-    var timeArr = []
-    activeData.forEach(function (n, i, a) {
-      if (n['flag'] === 'date') dateArr.push(n)
-      else if (n['flag'] === 'time') timeArr.push(n)
-      else timeArr.push(n)
-    })
-    dateArr.forEach(function (n, i, a) {
-      if (i === dateArr.length - 1) {
-        activeText += n['key']
-      } else {
-        activeText += n['key'] + '-'
+    var currentMaxday = new Date(nowDate.getFullYear(), nowDate.getMonth() + 1, 0).getDate()
+    if (s.params.daysData) {
+      s.days = s.params.daysData
+    } else {
+      for (var dd = 1; dd <= currentMaxday; dd++) {
+        var _dd = dd.toString().length === 1 ? '0' + dd : dd
+        s.days.push({
+          'key': '' + _dd,
+          'value': _dd + s.params.ddUnit,
+          'flag': 'date'
+        })
       }
-    })
-    if (activeText !== '') {
-      activeText += ' '
     }
-    timeArr.forEach(function (n, i, a) {
-      if (i === timeArr.length - 1) {
-        activeText += n['key']
-      } else {
-        activeText += n['key'] + ':'
+
+    function updateDays(year, month, defaultDay) {
+      var maxDay = new Date(year, month, 0).getDate()
+      s.days = []
+      for (var dd = 1; dd <= maxDay; dd++) {
+        var _dd = dd.toString().length === 1 ? '0' + dd : dd
+        s.days.push({
+          'key': '' + _dd,
+          'value': _dd + s.params.ddUnit,
+          'flag': 'date'
+        })
       }
-    })
-    return activeText
-  }
-
-  /* ----------------
-  Control
-  ---------------- */
-
-  // 添加数据
-  function addMonthSlot () {
-    s.addSlot(s.years, s.params.defaultYear, s.params.yearClass)
-    s.addSlot(s.months, s.params.defaultMonth, s.params.monthClass)
-  }
-
-  function addDateSlot () {
-    addMonthSlot()
-    s.addSlot(s.days, s.params.defaultDay, s.params.dayClass)
-  }
-
-  function addTimeSlot () {
-    s.addSlot(s.hours, s.params.defaultHour, s.params.hourClass)
-    s.addSlot(s.minutes, s.params.defaultMinute, s.params.minuteClass)
-  }
-
-  function addDateTime () {
-    addDateSlot()
-    addTimeSlot()
-  }
-
-  function initSlots () {
-    switch (s.params.viewType) {
-      case 'date':
-        addDateSlot()
-        break
-      case 'month':
-        addMonthSlot()
-        break
-      case 'time':
-        addTimeSlot()
-        break
-      case 'datetime':
-        addDateTime()
-        break
+      var defaultKey = defaultDay
+      if (s.days.length < defaultDay) defaultKey = s.days[s.days.length - 1]['key']
+      s.replaceSlot(2, s.days, defaultKey, s.params.dayClass) // 修改第三项
     }
-  }
-  s.update = function () {
-    s.clearSlots()
+
+    // 时
+    s.hours = []
+    if (s.params.hoursData) {
+      s.hours = s.params.hoursData
+    } else {
+      for (var hh = 0; hh <= 23; hh++) {
+        var _hh = hh.toString().length === 1 ? '0' + hh : hh
+        s.hours.push({
+          'key': '' + _hh,
+          'value': _hh + s.params.hhUnit,
+          'flag': 'time'
+        })
+      }
+    }
+
+    // 分
+    s.minutes = []
+    if (s.params.minutesData) {
+      s.minutes = s.params.minutesData
+    } else {
+      for (var mm = 0; mm <= 59; mm++) {
+        var _mm = mm.toString().length === 1 ? '0' + mm : mm
+        s.minutes.push({
+          'key': '' + _mm,
+          'value': _mm + s.params.mmUnit,
+          'flag': 'time'
+        })
+      }
+    }
+
+    /* ----------------
+    Method
+    ---------------- */
+    function getActiveText(activeData) {
+      var activeText = ''
+      var dateArr = []
+      var timeArr = []
+      activeData.forEach(function (n, i, a) {
+        if (n['flag'] === 'date') dateArr.push(n)
+        else if (n['flag'] === 'time') timeArr.push(n)
+        else timeArr.push(n)
+      })
+      dateArr.forEach(function (n, i, a) {
+        if (i === dateArr.length - 1) {
+          activeText += n['key']
+        } else {
+          activeText += n['key'] + '-'
+        }
+      })
+      if (activeText !== '') {
+        activeText += ' '
+      }
+      timeArr.forEach(function (n, i, a) {
+        if (i === timeArr.length - 1) {
+          activeText += n['key']
+        } else {
+          activeText += n['key'] + ':'
+        }
+      })
+      return activeText
+    }
+
+    /* ----------------
+    Control
+    ---------------- */
+
+    // 添加数据
+    function addMonthSlot() {
+      s.addSlot(s.years, s.params.defaultYear, s.params.yearClass)
+      s.addSlot(s.months, s.params.defaultMonth, s.params.monthClass)
+    }
+
+    function addDateSlot() {
+      addMonthSlot()
+      s.addSlot(s.days, s.params.defaultDay, s.params.dayClass)
+    }
+
+    function addTimeSlot() {
+      s.addSlot(s.hours, s.params.defaultHour, s.params.hourClass)
+      s.addSlot(s.minutes, s.params.defaultMinute, s.params.minuteClass)
+    }
+
+    function addDateTime() {
+      addDateSlot()
+      addTimeSlot()
+    }
+
+    function initSlots() {
+      switch (s.params.viewType) {
+        case 'date':
+          addDateSlot()
+          break
+        case 'month':
+          addMonthSlot()
+          break
+        case 'time':
+          addTimeSlot()
+          break
+        case 'datetime':
+          addDateTime()
+          break
+      }
+    }
+    s.update = function () {
+      s.clearSlots()
+      initSlots()
+    }
     initSlots()
+    return s
   }
-  initSlots()
-  return s
-}
 })(window, document, undefined);
