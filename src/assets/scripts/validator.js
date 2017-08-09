db6fe1f4066e0578eac76f00e6a7c15ba66ea864
colaboy <49201650@qq.com>
2017-08-09 14:01:19 +0800

    修改titlebar对齐

--- src/assets/scripts/validator.js
+++ src/assets/scripts/validator.js
@@ -1,121 +1,123 @@
-//Validator 表单验证 (require safelvl.js)
-(function(window,document,undefined){
-	/********************验证策略类********************/
-	window.Rules={
-		required:function(value,errorMsg){
-			if(!/.+/.test(value)){
-				return errorMsg;
-			}
-		},
-		integer:function(value,errorMsg){
-			if(!/^-?\d*$/.test(value)){
-				return errorMsg;
-			}
-		},
-		positiveInteger:function(value,errorMsg){
-			if(value.length != 0 && !/^[1-9]{1,}[0-9]*$/.test(value)){
-				return errorMsg;
-			}
-		},
-		negativeInteger:function(value,errorMsg){
-			if(value.length != 0 && !/^-[1-9]{1,}[0-9]*$/.test(value)){
-				return errorMsg;
-			}
-		},
-		username:function(value,errorMsg){
-			if(!/^[\w]*$/.test(value)){
-				return errorMsg;
-			}
-		},
-		password:function(value,errorMsg){
-			if(!/^[0-9_a-zA-Z-~!@#$]*$/.test(value)){
-				return errorMsg;
-			}
-		},
-		mail:function(value,errorMsg){
-			if(!/^(\w+@\w+\.[\.\w]+)?$/.test(value)){
-				return errorMsg;
-			}
-		},
-		phone:function(value,errorMsg){
-			if(!/^([1][34578][0-9]{9})?$/.test(value)){
-				return errorMsg;
-			}
-		},
-		chinese:function(value,errorMsg){
-			if(!/^[\u4E00-\u9FA5]*$/.test(value)){
-				return errorMsg;
-			}
-		},
-		specialchar:function(value,errorMsg){
-			if(!/^([\u4e00-\u9fa5]*|[a-zA-Z0-9]*)$/.test(value)){
-				return errorMsg;
-			}
-		},
-		number:function(value,errorMsg){
-			if(value.length != 0 && !Number(value)){
-				return errorMsg;
-			}
-		},
-		minNumber:function(value,min,errorMsg){
-			if(value.length != 0 && value*1 < min*1){
-				return errorMsg;
-			}
-		},
-		maxNumber:function(value,max,errorMsg){
-			if(value.length != 0 && value*1 > max*1){
-				return errorMsg;
-			}
-		},
-		minLength:function(value,length,errorMsg){
-			if(value.length != 0 && value.length < length){
-				return errorMsg;
-			}
-		},
-		maxLength:function(value,length,errorMsg){
-			if(value.length != 0 && value.length > length){
-				return errorMsg;
-			}
-		},
-		compare:function(value1,value2,errorMsg){
-			if(value1 != value2){
-				return errorMsg;
-			}
-		},
-		safeLvl:function(value,lvl,errorMsg){
-	        var valLvl=SafeLvl.check(value);
-	        if(value.length != 0 && valLvl < lvl){
-	        	return errorMsg;
-	        }
-	    }
-	};
+// Validator 表单验证 (require safelvl.js)
 
-	/********************Validator类********************/
-	window.Validator=function(){
-		var s=this;
-		s.caches=[];
-		s.add=function(field,strategies){
-			var self=this;
-			for(var i=0,strategy;strategy=strategies[i++];){
-				(function(strategy){
-					var ruleArray=strategy.rule.split(":");
-					var errorMsg=strategy.errorMsg;
+var Validator = function () {
+  /* ------------------------
+  验证策略类
+  ------------------------ */
+  var Rules = {
+    required: function (value, errorMsg) {
+      if (!/.+/.test(value)) {
+        return errorMsg
+      }
+    },
+    integer: function (value, errorMsg) {
+      if (!/^-?\d*$/.test(value)) {
+        return errorMsg
+      }
+    },
+    positiveInteger: function (value, errorMsg) {
+      if (value.length != 0 && !/^[1-9]{1,}[0-9]*$/.test(value)) {
+        return errorMsg
+      }
+    },
+    negativeInteger: function (value, errorMsg) {
+      if (value.length != 0 && !/^-[1-9]{1,}[0-9]*$/.test(value)) {
+        return errorMsg
+      }
+    },
+    username: function (value, errorMsg) {
+      if (!/^[\w]*$/.test(value)) {
+        return errorMsg
+      }
+    },
+    password: function (value, errorMsg) {
+      if (!/^[0-9_a-zA-Z-~!@#$]*$/.test(value)) {
+        return errorMsg
+      }
+    },
+    mail: function (value, errorMsg) {
+      if (!/^(\w+@\w+\.[\.\w]+)?$/.test(value)) {
+        return errorMsg
+      }
+    },
+    phone: function (value, errorMsg) {
+      if (!/^([1][34578][0-9]{9})?$/.test(value)) {
+        return errorMsg
+      }
+    },
+    chinese: function (value, errorMsg) {
+      if (!/^[\u4E00-\u9FA5]*$/.test(value)) {
+        return errorMsg
+      }
+    },
+    specialchar: function (value, errorMsg) {
+      if (!/^([\u4e00-\u9fa5]*|[a-zA-Z0-9]*)$/.test(value)) {
+        return errorMsg
+      }
+    },
+    number: function (value, errorMsg) {
+      if (value.length != 0 && !Number(value)) {
+        return errorMsg
+      }
+    },
+    minNumber: function (value, min, errorMsg) {
+      if (value.length != 0 && value * 1 < min * 1) {
+        return errorMsg
+      }
+    },
+    maxNumber: function (value, max, errorMsg) {
+      if (value.length != 0 && value * 1 > max * 1) {
+        return errorMsg
+      }
+    },
+    minLength: function (value, length, errorMsg) {
+      if (value.length != 0 && value.length < length) {
+        return errorMsg
+      }
+    },
+    maxLength: function (value, length, errorMsg) {
+      if (value.length != 0 && value.length > length) {
+        return errorMsg
+      }
+    },
+    compare: function (value1, value2, errorMsg) {
+      if (value1 != value2) {
+        return errorMsg
+      }
+    },
+    safeLvl: function (value, lvl, errorMsg) {
+      var valLvl = SafeLvl.check(value)
+      if (value.length != 0 && valLvl < lvl) {
+        return errorMsg
+      }
+    }
+  }
+  /* ------------------------
+    Validator类
+    ------------------------ */
+  var s = this
+  s.caches = []
+  s.add = function (field, strategies) {
+    var self = this
+    for (var i = 0, strategy; strategy = strategies[i++];) {
+      (function (strategy) {
+        var ruleArray = strategy.rule.split(":")
+        var errorMsg = strategy.errorMsg
 
-					s.caches.push(function(){
-						var ruleName=ruleArray.shift();
-						ruleArray.unshift(field instanceof Object?field.value:field);
-						ruleArray.push(errorMsg);//此时ruleArray的值为 ruleValue,fieldValue,errorMsg
-						var ruleErrorMsg=Rules[ruleName].apply(null,ruleArray);
-						if(ruleErrorMsg) return {field:field,msg:ruleErrorMsg};
-					});
-				})(strategy);
-			}
-		};
-		s.start=function(){
-			for(var i=0,valiFn;valiFn=s.caches[i++];){
-				var error=valiFn();
-				if(error)return error;
-			}
-		};
-	};
-})(window,document,undefined);
+        s.caches.push(function () {
+          var ruleName = ruleArray.shift()
+          ruleArray.unshift(field instanceof Object ? field.value : field)
+          ruleArray.push(errorMsg) // 此时ruleArray的值为 ruleValue,fieldValue,errorMsg
+          var ruleErrorMsg = Rules[ruleName].apply(null, ruleArray)
+          if (ruleErrorMsg) return { field: field, msg: ruleErrorMsg }
+        })
+      })(strategy)
+    }
+  }
+  s.start = function () {
+    for (var i = 0, valiFn; valiFn = s.caches[i++];) {
+      var error = valiFn()
+      if (error) return error
+    }
+  }
+};
