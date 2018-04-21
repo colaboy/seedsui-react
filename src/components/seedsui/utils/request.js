5c46d8c2132cdfefbf82a5e92f11608d4230ee25
王明柱 <wangmingzhu@wangmingzhudeMacBook-Pro.local>
2018-04-21 15:48:10 +0800

    不好意思,后面会将此产品转移到seedsui-react上去了

--- src/components/seedsui/utils/request.js
+++
@@ -1,9 +0,0 @@
-var Request = {
-  getParameter: function (argName) {
-    var href = decodeURI(window.location.href)
-    var param = href.match(new RegExp('[\\?\\&]' + argName + '=([^\\&]*)(\\&?)', 'i'))
-    return param ? decodeURI(param[1]) : param
-  }
-}
-
-;//export default Request
