9c1136163a80037f74694ce6f2334415dfde658a
colaboy <49201650@qq.com>
2022-11-22 20:59:02 +0800

    修改Input.Text

--- src/Input/demos/demo1.tsx
+++ src/Input/demos/demo1.tsx
@@ -1,18 +1,13 @@
 import React from 'react'
+import { Input } from 'seedsui-react'
 
-const titleStyle = {
-  padding: '12px 12px 8px',
-  color: '#697b8c',
-  fontSize: '14px',
-  backgroundColor: 'rgba(250,251,252)'
-}
-const buttonStyle = {
-  margin: '8px'
-}
 export default () => {
   return (
     <>
-      <div style={titleStyle}>填充模式</div>
+      <Input.Text
+        readOnly
+        value="中华人民共和国中华人民共和国中华人民共和国中华人民共和国中华人民共和国中华人民共和国"
+      />
     </>
   )
 }
