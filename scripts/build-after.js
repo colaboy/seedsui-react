55f39fbd4cd17e0052657b11ef928d5ac1ef308c
wangmingzhu <wangmingzhu@qince.com>
2023-01-03 11:02:40 +0800

    fixed build error issues

--- scripts/build-after.js
+++ scripts/build-after.js
@@ -5,7 +5,25 @@ const fooCatalog = `./dist/esm`
 // out目录
 const outCatalog = `./lib`
 
-// 复制目录
+// 删除目录
+if (fs.existsSync(outCatalog)) {
+  try {
+    fs.rmSync(outCatalog, { recursive: true })
+    console.log('successfully deleted' + outCatalog)
+  } catch (error) {
+    console.error('delete was an error:', error.message)
+  }
+}
+
+// 创建目录
+try {
+  fs.mkdirSync(outCatalog)
+  console.log('successfully create' + outCatalog)
+} catch (error) {
+  console.error('create was an error:', error.message)
+}
+
+// // 复制目录
 fs.cp(fooCatalog, outCatalog, { recursive: true }, (err) => {
   if (err) {
     console.error(err)
