7cb7f4c62e3e5c9bd955c1be76535d062309239a
wangmingzhu <wangmingzhu@qince.com>
2023-05-24 09:26:37 +0800

    docs: add Checkbox status demo

--- src/Checkbox/demos/demo1.jsx
+++ src/Checkbox/demos/demo1.jsx
@@ -7,7 +7,14 @@ export default () => {
   return (
     <>
       <Checkbox checked={value} onChange={setValue}>
-        选项1
+        普通
+      </Checkbox>
+      <Checkbox checked={true}>选中</Checkbox>
+      <Checkbox checked={false} disabled>
+        普通-禁用
+      </Checkbox>
+      <Checkbox checked={true} disabled>
+        选中-禁用
       </Checkbox>
     </>
   )
