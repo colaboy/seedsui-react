e711723de421d5277ca158acc60b50ba7c54669a
wangmingzhu <wangmingzhu@qince.com>
2023-07-18 14:34:21 +0800

    fix: fixed the Location.Combo url issues

--- src/Location/Modal/addHistory.js
+++ src/Location/Modal/addHistory.js
@@ -1,17 +1,22 @@
 // 页面模式: 显示页面
 function addHistory({ modal, loaded }) {
   if (modal !== 'page' || !loaded || window.location.href.indexOf('modalPage=1') !== -1) return
+
+  let url = window.location.href
+  // url最后一个字符为?号时删除?号
+  url = url.indexOf('?') === url.length - 1 ? url.substring(0, url.length - 1) : url
+  // url最后一个字符为&号时删除&号
+  url = url.indexOf('&') === url.length - 1 ? url.substring(0, url.length - 1) : url
+
+  // url分割符
+  let split = url.indexOf('?') === -1 ? '?' : '&'
+
   window.history.pushState(
     {
       modalPage: '1'
     },
     '',
-    `${window.location.pathname + window.location.search}${
-      window.location.href.indexOf('?') === -1 ||
-      window.location.href.indexOf('?') === window.location.href.length - 1
-        ? '?'
-        : '&'
-    }modalPage=1`
+    `${url}${split}modalPage=1`
   )
 }
 
