97c2a583b9f52a9a60c9c762a44fe16d71666a4a
wangmingzhu <wangmingzhu@qince.com>
2023-08-21 20:55:00 +0800

    feat: devlop the Button new variable

--- src/Button/demos/demo1.jsx
+++ src/Button/demos/demo1.jsx
@@ -14,12 +14,87 @@ export default () => {
   return (
     <>
       <div style={titleStyle}>填充模式</div>
-      <Button>(Default)</Button>
+      <Button style={buttonStyle}>default</Button>
+      <Button className="disabled" style={buttonStyle}>
+        default disabled
+      </Button>
+      <br />
       <Button className="primary" style={buttonStyle}>
         primary
       </Button>
+      <Button className="primary disabled" style={buttonStyle}>
+        primary disabled
+      </Button>
+      <br />
+      <Button className="link" style={buttonStyle}>
+        link
+      </Button>
+      <Button className="link disabled" style={buttonStyle}>
+        link disabled
+      </Button>
+      <br />
+      <Button className="warning" style={buttonStyle}>
+        warning
+      </Button>
+      <Button className="warning disabled" style={buttonStyle}>
+        warning disabled
+      </Button>
+      <br />
+      <Button className="danger" style={buttonStyle}>
+        danger
+      </Button>
+      <Button className="danger disabled" style={buttonStyle}>
+        danger disabled
+      </Button>
+      <br />
+      <Button className="success" style={buttonStyle}>
+        success
+      </Button>
+      <Button className="success disabled" style={buttonStyle}>
+        success disabled
+      </Button>
+
+      <div style={titleStyle}>边框模式</div>
+      <Button className="outline" style={buttonStyle}>
+        default outline
+      </Button>
+      <Button className="outline disabled" style={buttonStyle}>
+        default disabled
+      </Button>
+      <br />
       <Button className="primary outline" style={buttonStyle}>
-        primary outline
+        primary
+      </Button>
+      <Button className="primary outline disabled" style={buttonStyle}>
+        primary disabled
+      </Button>
+      <br />
+      <Button className="link outline" style={buttonStyle}>
+        link
+      </Button>
+      <Button className="link outline disabled" style={buttonStyle}>
+        link disabled
+      </Button>
+      <br />
+      <Button className="warning outline" style={buttonStyle}>
+        warning
+      </Button>
+      <Button className="warning outline disabled" style={buttonStyle}>
+        warning disabled
+      </Button>
+      <br />
+      <Button className="danger outline" style={buttonStyle}>
+        danger
+      </Button>
+      <Button className="danger outline disabled" style={buttonStyle}>
+        danger disabled
+      </Button>
+      <br />
+      <Button className="success outline" style={buttonStyle}>
+        success
+      </Button>
+      <Button className="success outline disabled" style={buttonStyle}>
+        success disabled
       </Button>
 
       <div style={titleStyle}>块级按钮</div>
@@ -45,30 +120,21 @@ export default () => {
       </Button>
 
       <div style={titleStyle}>按钮颜色</div>
-      <Button>(Default)</Button>
+      <Button style={buttonStyle}>default</Button>
       <Button className="primary" style={buttonStyle}>
         primary
-      </Button>
-      <Button className="cancel" style={buttonStyle}>
-        cancel
-      </Button>
-      <Button className="submit" style={buttonStyle}>
-        submit
-      </Button>
-      <Button className="info" style={buttonStyle}>
-        info
       </Button>
       <Button className="link" style={buttonStyle}>
         link
       </Button>
-      <Button className="warn" style={buttonStyle}>
-        warn
+      <Button className="warning" style={buttonStyle}>
+        warning
+      </Button>
+      <Button className="danger" style={buttonStyle}>
+        danger
       </Button>
       <Button className="success" style={buttonStyle}>
         success
-      </Button>
-      <Button className="disabled" style={buttonStyle}>
-        disabled
       </Button>
     </>
   )
