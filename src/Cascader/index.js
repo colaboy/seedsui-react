466e7abe465d2cf800d27df6671df5866822c793
wangmingzhu <wangmingzhu@qince.com>
2023-09-20 13:54:45 +0800

    feat: devlop the Cascader

--- src/Cascader/index.js
+++ src/Cascader/index.js
@@ -1,12 +1,16 @@
 import Combo from './Combo'
 import Modal from './Modal'
+import Main from './Main'
 import DistrictCombo from './DistrictCombo'
 import DistrictModal from './DistrictModal'
+import DistrictMain from './DistrictMain'
 
 // eslint-disable-next-line
 export default {
   Combo: Combo,
   Modal: Modal,
+  Main: Main,
   DistrictCombo: DistrictCombo,
-  DistrictModal: DistrictModal
+  DistrictModal: DistrictModal,
+  DistrictMain: DistrictMain
 }
