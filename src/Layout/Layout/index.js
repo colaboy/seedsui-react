9c71145c3a7b3113aabc63919502145457b25d68
wangmingzhu <wangmingzhu@qince.com>
2023-09-25 19:25:11 +0800

    feat(5.7.68): devlop the new Layout

--- src/Layout/Layout/index.js
+++ src/Layout/Layout/index.js
@@ -1,31 +1,7 @@
 import React, { useImperativeHandle, forwardRef, useRef, useEffect } from 'react'
-import Device from './../../Device'
 
-const Layout = forwardRef(({ animation, children, safeArea, ...props }, ref) => {
+const Layout = forwardRef(({ animation, children, ...props }, ref) => {
   const rootRef = useRef(null)
-
-  // 安全区域，Layout全屏时增加安全区域（仅对苹果的安全区域生效）
-  if (
-    Device.os === 'ios' &&
-    Device.platform === 'wq' &&
-    Device.compareVersion(Device.platformVersion, '7.1.65') >= 0
-  ) {
-    // 自动上下
-    if (safeArea === 'auto') {
-      // eslint-disable-next-line
-      safeArea = 'border-top border-bottom'
-    }
-    // 自动下
-    if (safeArea === 'auto-bottom') {
-      // eslint-disable-next-line
-      safeArea = 'border-bottom'
-    }
-    // 自动上
-    if (safeArea === 'auto-top') {
-      // eslint-disable-next-line
-      safeArea = 'border-top'
-    }
-  }
 
   // 节点
   useImperativeHandle(ref, () => {
@@ -57,9 +33,7 @@ const Layout = forwardRef(({ animation, children, safeArea, ...props }, ref) => 
   return (
     <section
       {...props}
-      className={`layout safe-area${safeArea ? ' ' + safeArea : ''}${
-        props.className ? ' ' + props.className : ''
-      }`}
+      className={`layout${props.className ? ' ' + props.className : ''}`}
       data-animation={animation}
       ref={rootRef}
     >
