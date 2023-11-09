9fdb59ed2d41ca8d59be645fa20445a3dd5a5d04
wangmingzhu <wangmingzhu@qince.com>
2023-11-09 13:56:46 +0800

    feat: add umd

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -4,5 +4,5 @@ export default defineConfig({
   // more father config: https://github.com/umijs/father/blob/master/docs/config.md
   esm: { output: 'dist' },
   // 因react-sortablejs 6.1.4 需要react18所以package.json里不能包含此库, 但umd打包需要先安装一下
-  umd: { output: 'umd' }
+  umd: { output: 'umd', name: 'seedsui-react' }
 })
