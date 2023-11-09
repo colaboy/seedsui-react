d26925ba241a1c8183d4b90f2a9bb6d12fe5c478
wangmingzhu <wangmingzhu@qince.com>
2023-11-09 10:59:37 +0800

    feat: support umd

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,6 +1,8 @@
-import { defineConfig } from 'father';
+import { defineConfig } from 'father'
 
 export default defineConfig({
   // more father config: https://github.com/umijs/father/blob/master/docs/config.md
   esm: { output: 'dist' },
-});
+  // 因react-sortablejs 6.1.4 需要react18所以package.json里不能包含此库, 但umd打包需要先安装一下
+  umd: { output: 'umd' }
+})
