f7252b6145a3647e45ba5744ccad7ad71c441fc2
wangmingzhu <wangmingzhu@qince.com>
2024-01-16 15:57:26 +0800

    feat(5.7.95): devlop the Location

--- src/Location/index.d.ts
+++ src/Location/index.d.ts
@@ -1,7 +1,10 @@
 import Combo from './Combo'
 import Modal from './Modal'
+import Main from './Main'
 
+// eslint-disable-next-line
 export default {
   Combo: Combo,
-  Modal: Modal
+  Modal: Modal,
+  Main: Main
 }
