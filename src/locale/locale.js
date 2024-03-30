ae89837b533bc8f7921f755f4de2aee6709604e2
wangmingzhu <wangmingzhu@qince.com>
2024-03-30 16:14:06 +0800

    feat: devlop the locale

--- src/locale/locale.js
+++ src/locale/locale.js
@@ -10,26 +10,30 @@ import Interpolate from './Interpolate'
  */
 // eslint-disable-next-line
 export default function (remark, key, variable, dangerouslyHTML) {
-  let locale = window.localeData
+  let localeData = window.localeData
   // 如果没有参数, 则返回国际化数据
   if (Array.from(arguments).length === 0) {
-    return locale
+    return localeData
   }
   // 渲染国际化数据中对应的值
-  if (key && locale) {
-    let value = locale[key]
+  if (key && localeData) {
+    let value = localeData[key]
+
+    // 没有对应值，或者值不是字符串类型，则返回remark
+    if (!value || typeof value !== 'string') {
+      return remark
+    }
+
     // 构建变量映射: 将[variable0, variable1]转换成{'0': variable0, '1': variable1}
     let variableMap = {}
-    if (value && variable && Array.isArray(variable) && variable.length) {
+    if (variable && Array.isArray(variable) && variable.length) {
       for (let [index, variableItem] of variable.entries()) {
         variableMap[`${index}`] = variableItem
       }
     }
 
-    // key的值value必须为string类型
-    if (typeof value === 'string') {
-      return Interpolate.variable(value, variableMap, dangerouslyHTML)
-    }
+    // 将variableMap替换value的值中的变量{0}、{1}等
+    return Interpolate.variable(value, variableMap, dangerouslyHTML)
   }
   // 没有国际化的值, 则渲染remark
   if (remark) return Interpolate.remark(remark, dangerouslyHTML)
