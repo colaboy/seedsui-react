a988803733e88344d07abeadf6e8eceede0e0d91
wangmingzhu <wangmingzhu@qince.com>
2024-05-06 14:00:47 +0800

    fix: fixed the RangeModal save issues

--- src/DatePicker/RangeModal/index.js
+++ src/DatePicker/RangeModal/index.js
@@ -1,5 +1,7 @@
 // require PrototypeDate.js和PrototypeString.js
 import React, { forwardRef } from 'react'
+import { validateRange } from './../utils'
+
 import RangeMain from './../RangeMain'
 import BaseModal from './../../Select/Modal'
 // 测试使用
@@ -91,7 +93,29 @@ const RangeModal = (
       // Main: common
       value={value}
       allowClear={allowClear}
-      onBeforeChange={onBeforeChange}
+      onBeforeChange={async (newValue) => {
+        // 只能校验min和max, 因为不知道用户此刻选中的的项是哪项
+        let goOn = await validateRange(newValue, {
+          type,
+          min,
+          max,
+          // dateRangeLimit:
+          //   activeKey && typeof ranges[activeKey] === 'number' ? ranges[activeKey] : null,
+          onError,
+          onBeforeChange
+          // ranges,
+          // activeKey: activeKey
+        })
+        if (goOn === false) return false
+
+        // 修改值
+        if (Array.isArray(goOn) && goOn.length === 2) {
+          // eslint-disable-next-line
+          newValue = goOn
+        }
+
+        return newValue
+      }}
       onChange={onChange}
       // Main: Picker Control properties
       defaultPickerValue={defaultPickerValue}
