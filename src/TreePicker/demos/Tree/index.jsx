84c476a921820b6cd65b16e5e61ca5bca26913e6
wangmingzhu <wangmingzhu@qince.com>
2024-05-23 15:50:44 +0800

    feat: develop the demo

--- src/TreePicker/demos/Tree/index.jsx
+++ src/TreePicker/demos/Tree/index.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react'
 import treeData from './../simpleData.js'
 // import treeData from './../data.js'
-import { Input, TreePicker, HighlightKeyword, locale } from 'seedsui-react'
+import { Input, TreePicker, Layout, locale } from 'seedsui-react'
 
 export default () => {
   const [data, setData] = useState(treeData)
@@ -17,89 +17,93 @@ export default () => {
   const treeRef = useRef(null)
 
   return (
-    <>
-      <Input.Text
-        type="search"
-        placeholder={locale('搜索', 'input_search_placeholder')}
-        onChange={(keyword) => {
-          if (window.timeout) window.clearTimeout(window.timeout)
-          window.timeout = setTimeout(() => {
-            treeRef.current.search(keyword)
-          }, 500)
-        }}
-        className="treepicker-search-input"
-        allowClear={true}
-      />
-      <TreePicker.Tree
-        ref={treeRef}
-        placeholder="Please select"
-        value={value}
-        list={data}
-        multiple={true}
-        // 仅允许选中末级
-        // onlyLeafCheck
-        // 级联 true: 不级联, false: 级联, children: 子级不级联父级
-        // checkStrictly={'children'}
-        // 定义选中项回填的方式: leaf仅显示所有末级节点; parent仅显示父级节点
-        showCheckedStrategy="leaf"
-        // 保留不在树结构中的value
-        preserveValue
-        // 启用半选, 将会返回半选节点
-        enableHalfChecked
-        // checkable={false}
-        // itemRender={(item, { keyword }) => {
-        //   return (
-        //     <div className="flex flex-middle">
-        //       {item.isLeaf && (
-        //         <img
-        //           className="size36"
-        //           src="https://image-test.waiqin365.com/imobii_portal/images/icon/default-face-small.png"
-        //           alt=""
-        //           style={{ borderRadius: '100%', marginRight: '4px' }}
-        //         />
-        //       )}
-        //       <HighlightKeyword text={item.name} keyword={keyword} />
-        //     </div>
-        //   )
-        // }}
-        onChange={(newValue) => {
-          console.log('checked:', newValue)
-          setValue(newValue)
-        }}
-        onSelect={(item) => {
-          console.log('select:', item)
-        }}
-        onVisibleChange={(visible) => {
-          console.log('visible:', visible)
-        }}
-        loadData={(node) => {
-          return new Promise((resolve) => {
-            // 假设西城区有子节点
-            if (node?.id !== '110102') {
+    <Layout className="full">
+      <Layout.Header>
+        <Input.Text
+          type="search"
+          placeholder={locale('搜索', 'input_search_placeholder')}
+          onChange={(keyword) => {
+            if (window.timeout) window.clearTimeout(window.timeout)
+            window.timeout = setTimeout(() => {
+              treeRef.current.search(keyword)
+            }, 500)
+          }}
+          className="treepicker-search-input"
+          allowClear={true}
+        />
+      </Layout.Header>
+      <Layout.Main>
+        <TreePicker.Tree
+          ref={treeRef}
+          placeholder="Please select"
+          value={value}
+          list={data}
+          multiple={true}
+          // 仅允许选中末级
+          // onlyLeafCheck
+          // 级联 true: 不级联, false: 级联, children: 子级不级联父级
+          // checkStrictly={'children'}
+          // 定义选中项回填的方式: leaf仅显示所有末级节点; parent仅显示父级节点
+          showCheckedStrategy="leaf"
+          // 保留不在树结构中的value
+          preserveValue
+          // 启用半选, 将会返回半选节点
+          enableHalfChecked
+          // checkable={false}
+          // itemRender={(item, { keyword }) => {
+          //   return (
+          //     <div className="flex flex-middle">
+          //       {item.isLeaf && (
+          //         <img
+          //           className="size36"
+          //           src="https://image-test.waiqin365.com/imobii_portal/images/icon/default-face-small.png"
+          //           alt=""
+          //           style={{ borderRadius: '100%', marginRight: '4px' }}
+          //         />
+          //       )}
+          //       <HighlightKeyword text={item.name} keyword={keyword} />
+          //     </div>
+          //   )
+          // }}
+          onChange={(newValue) => {
+            console.log('checked:', newValue)
+            setValue(newValue)
+          }}
+          onSelect={(item) => {
+            console.log('select:', item)
+          }}
+          onVisibleChange={(visible) => {
+            console.log('visible:', visible)
+          }}
+          loadData={(node) => {
+            return new Promise((resolve) => {
+              // 假设西城区有子节点
+              if (node?.id !== '110102') {
+                resolve(null)
+                return
+              }
+              if (!node.children) {
+                data.setDeepTreeNodeProp(node.id, (item) => {
+                  item.isLoaded = true
+                  item.children = [
+                    {
+                      isLoaded: true,
+                      parentid: node.id,
+                      id: '1',
+                      name: '1111111'
+                    }
+                  ]
+                })
+
+                console.log('展开')
+                setData([...data])
+              }
+
               resolve(null)
-              return
-            }
-            if (!node.children) {
-              data.setDeepTreeNodeProp(node.id, (item) => {
-                item.isLoaded = true
-                item.children = [
-                  {
-                    isLoaded: true,
-                    parentid: node.id,
-                    id: '1',
-                    name: '1111111'
-                  }
-                ]
-              })
-
-              console.log('展开')
-              setData([...data])
-            }
-
-            resolve(null)
-          })
-        }}
-      />
-    </>
+            })
+          }}
+        />
+      </Layout.Main>
+    </Layout>
   )
 }
