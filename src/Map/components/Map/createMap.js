bdf591f9d4ee6cfabb90b0785c3e8cd47d673519
wangmingzhu <wangmingzhu@qince.com>
2024-06-12 14:38:44 +0800

    feat: develop the baiduMap

--- src/Map/components/Map/createMap.js
+++ src/Map/components/Map/createMap.js
@@ -4,12 +4,24 @@ function createMap(container, { center, zoom }) {
     return '请在Map组件需要使用APILoader包裹'
   }
 
-  // Init leaflet map
-  const map = window.L.map(container, {
-    attributionControl: false, // 隐藏版权控件
-    zoomControl: false // 隐藏放大缩小控件
-  }).setView([center?.latitude, center?.longitude], zoom)
+  let map = null
 
+  // 注意将map的crs赋值 crs: L.CRS.Baidu 详情请阅读示例页面
+  if (window.L.tileLayer.BMapLayer) {
+    map = L.map(container, {
+      crs: L.CRS.Baidu,
+      attributionControl: false,
+      center: [center?.latitude, center?.longitude],
+      zoom: zoom
+    })
+  }
+  // Init leaflet map
+  else {
+    map = window.L.map(container, {
+      attributionControl: false, // 隐藏版权控件
+      zoomControl: false // 隐藏放大缩小控件
+    }).setView([center?.latitude, center?.longitude], zoom)
+  }
   return map
 }
 
--- src/Map/components/TileLayer/createTileLayer.js
+++ src/Map/components/TileLayer/createTileLayer.js
@@ -7,7 +7,35 @@ async function createTileLayer(map) {
   }
   // Use bmap tileLayer
   if (window.L.tileLayer.BMapLayer) {
-    tileLayer = window.L.tileLayer.BMapLayer({ type: 'roadmap' }).addTo(map)
+    debugger
+    tileLayer = L.control
+      .layers(
+        {
+          百度地图: window.L.tileLayer.BMapLayer({ layer: 'vec' }).addTo(map),
+          百度卫星: window.L.tileLayer.BMapLayer({ layer: 'img' }),
+          '百度地图-大字体': window.L.tileLayer.BMapLayer({
+            layer: 'vec',
+            bigfont: true
+          }),
+          '百度卫星-大字体': window.L.tileLayer.BMapLayer({
+            layer: 'img',
+            bigfont: true
+          }),
+          '自定义样式-黑色地图': window.L.tileLayer.BMapLayer({
+            layer: 'custom',
+            customid: 'dark'
+          }),
+          '自定义样式-蓝色地图': window.L.tileLayer.BMapLayer({
+            layer: 'custom',
+            customid: 'midnight'
+          }) //自定义样式地图，customid可选值：dark,midnight,grayscale,hardedge,light,redalert,googlelite,grassgreen,pink,darkgreen,bluish
+        },
+        {
+          实时交通信息: window.L.tileLayer.BMapLayer({ layer: 'time' })
+        },
+        { position: 'topright' }
+      )
+      .addTo(map)
   }
   // Use openStreetMap tileLayer
   else {
