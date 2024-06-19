b6fb7a9c6017926b92efa6fc7e0e7b8adf3198bf
wangmingzhu <wangmingzhu@qince.com>
2024-06-19 13:35:22 +0800

    fix(5.8.31): fixed the the ts import Map

--- src/Map/index.d.ts
+++ src/Map/index.d.ts
@@ -1,2 +1,27 @@
-export { default } from './Map'
-export * from './Map'
+import getAddress from './utils/getAddress'
+import queryNearby from './utils/queryNearby'
+import APILoader from './components/APILoader'
+import MapContainer from './components/MapContainer'
+import ZoomControl from './components/ZoomControl'
+import SearchControl from './components/SearchControl'
+import CenterMarker from './components/CenterMarker'
+import Markers from './components/Markers'
+import LocationControl from './components/LocationControl'
+import NearbyControl from './components/NearbyControl'
+
+const Map = {
+  // utils
+  getAddress,
+  queryNearby,
+  // components
+  APILoader,
+  MapContainer,
+  ZoomControl,
+  SearchControl,
+  CenterMarker,
+  Markers,
+  LocationControl,
+  NearbyControl
+}
+
+export default Map
