fb349b110713ecf3a668af0d4d26d9aadbfeca82
wangmingzhu <wangmingzhu@qince.com>
2024-06-25 16:52:24 +0800

    feat(5.8.32): support the new property Loading.defaultProps for Loading.show

--- src/Loading/show.js
+++ src/Loading/show.js
@@ -3,7 +3,10 @@ import locale from './../locale'
 // 显示Loading
 // eslint-disable-next-line
 export default function (props) {
-  const { id, maskProps, captionProps, className, style, content, onVisibleChange } = props || {}
+  const { id, maskProps, captionProps, className, style, content, onVisibleChange } = {
+    ...(this?.defaultProps || {}),
+    ...(props || {})
+  }
   // 渲染
   function render() {
     let loadingId = id || '__SeedsUI_loading_el__'
@@ -32,22 +35,25 @@ export default function (props) {
       ;(document.getElementById('root') || document.body).appendChild(loadingDOM)
     }
 
-    // 更新dom
+    // 更新mask
     loadingDOM.setAttribute(
       'class',
       `loading-mask mask active${maskProps?.className ? ' ' + maskProps?.className : ''}`
     )
     loadingDOM.setAttribute('id', loadingId)
+    loadingDOM.setAttribute('style', '')
     for (let key in maskProps?.style || {}) {
       loadingDOM.style[key] = maskProps?.style[key]
     }
 
+    // 更新container
     let container = loadingDOM.querySelector('.loading-container')
     if (container) {
       container?.setAttribute(
         'class',
         `loading-container loading-floating animated${className ? ' ' + className : ''}`
       )
+      container?.setAttribute('style', '')
       for (let key in style || {}) {
         container.style[key] = style[key]
       }
