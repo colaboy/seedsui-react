106d0e2db4930c05abcf67f3e05355f5f5b53e61
wangmingzhu <wangmingzhu@qince.com>
2024-07-03 09:39:08 +0800

    feat: add harmony

--- src/Device/Device.js
+++ src/Device/Device.js
@@ -25,12 +25,16 @@ let Device = (function () {
   let osVersion = ''
   let androidExp = ua.match(/android\s*(\d*\.*\d*)/)
   let iosExp = ua.match(/cpu iphone os (.*?) like mac os/)
+  let harmonyExp = ua.match(/openharmony\s*(\d*\.*\d*)/)
   if (androidExp) {
     os = 'android'
     osVersion = androidExp[1]
   } else if (iosExp) {
     os = 'ios'
     osVersion = iosExp[1]
+  } else if (harmonyExp) {
+    os = 'harmony'
+    osVersion = harmonyExp[1]
   }
 
   // 平台
