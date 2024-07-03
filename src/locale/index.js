ade9300e94396041fb16d3277cc52e5df5638607
wangmingzhu <wangmingzhu@qince.com>
2024-07-03 15:28:39 +0800

    feat: export the locale

--- src/locale/index.js
+++ src/locale/index.js
@@ -1,17 +1,3 @@
 import locale from './locale.js'
-import en_US from './locales/en_US'
-import zh_HK from './locales/zh_HK'
-import zh_CN from './locales/zh_CN'
-
-// Open for external use: Get locale data
-locale.localeData = function (language) {
-  if (language === 'en_US') {
-    return en_US
-  }
-  if (language === 'zh_HK') {
-    return zh_HK
-  }
-  return zh_CN
-}
 
 export default locale
