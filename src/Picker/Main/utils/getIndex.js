05c9ff1a650ad531a2786d874976e74d80fc857a
wangmingzhu <wangmingzhu@qince.com>
2024-11-18 14:03:06 +0800

    feat: develop minuteStep and hourStep

--- src/Picker/Main/utils/getIndex.js
+++ src/Picker/Main/utils/getIndex.js
@@ -1,6 +1,8 @@
 // 获取当前位置
 function getIndex(activeOption, list) {
   if (!activeOption?.id || !Array.isArray(list) || !list.length) return 0
-  return list.findIndex((item) => item.id === activeOption.id) || 0
+  let activeIndex = list.findIndex((item) => item.id === activeOption.id)
+  // 未找到匹配项, 默认选中第一项
+  return activeIndex <= 0 ? 0 : activeIndex
 }
 export default getIndex
