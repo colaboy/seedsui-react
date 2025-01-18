8aefefd60bf97fdb5ea5918062f98cfd68e683b5
wangmingzhu <wangmingzhu@qince.com>
2025-01-18 13:52:09 +0800

    feat: modify getKeywordIdsByPinyin

--- src/components/TreePicker/Tree/utils/getKeywordIdsByPinyin.js
+++ src/components/TreePicker/Tree/utils/getKeywordIdsByPinyin.js
@@ -2,7 +2,9 @@
 function getKeywordIdsByPinyin(keyword, name) {
   if (!name || typeof name !== 'string') return ''
   if (!keyword) return name
-  let pinyinIndex = name?.toPinyin?.()?.indexOf(keyword.toLowerCase())
+  // 转成拼音
+  let namePinyin = name
+  let pinyinIndex = namePinyin?.indexOf(keyword.toLowerCase())
   if (pinyinIndex !== -1) {
     return name
   }
--- src/components/TreePicker/Tree/utils/index.js
+++ src/components/TreePicker/Tree/utils/index.js
@@ -2,7 +2,7 @@ import getCheckedKeysProp from './getCheckedKeysProp'
 import getAllExpandedKeys from './getAllExpandedKeys'
 import getExpandedKeys from './getExpandedKeys'
 import getKeywordIds from './getKeywordIds'
-import getKeywordIdsByPinyin from './getKeywordIdsByPinyin'
+// import getKeywordIdsByPinyin from './getKeywordIdsByPinyin'
 import getLoadedKeys from './getLoadedKeys'
 import getTreeData from './getTreeData'
 import filterValue from './filterValue'
@@ -12,7 +12,7 @@ export {
   getAllExpandedKeys,
   getExpandedKeys,
   getKeywordIds,
-  getKeywordIdsByPinyin,
+  // getKeywordIdsByPinyin,
   getLoadedKeys,
   getTreeData,
   filterValue
