148c28e722dcf8c3fe455ae5cf70eec7c4f0b8f4
wangmingzhu <wangmingzhu@qince.com>
2025-01-24 17:59:56 +0800

    feat: combine Button.less and Mark.less

--- src/components/Mark/demos/demo1.jsx
+++ src/components/Mark/demos/demo1.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Mark } from 'seedsui-react'
+import { Layout, Mark, Divider } from 'seedsui-react'
 
 const titleStyle = {
   padding: '12px 12px 8px',
@@ -12,66 +12,68 @@ const buttonStyle = {
 }
 export default () => {
   return (
-    <>
-      <div style={titleStyle}>填充模式</div>
-      <Mark className="default" style={buttonStyle}>
-        default
-      </Mark>
-      <Mark className="primary" style={buttonStyle}>
-        primary
-      </Mark>
-      <Mark className="link" style={buttonStyle}>
-        link
-      </Mark>
-      <Mark className="warning" style={buttonStyle}>
-        warning
-      </Mark>
-      <Mark className="danger" style={buttonStyle}>
-        danger
-      </Mark>
-      <Mark className="success" style={buttonStyle}>
-        success
-      </Mark>
+    <Layout className="full">
+      <Layout.Main>
+        <Divider>Fill Mode</Divider>
+        <Mark className="default" style={buttonStyle}>
+          default
+        </Mark>
+        <Mark className="primary" style={buttonStyle}>
+          primary
+        </Mark>
+        <Mark className="link" style={buttonStyle}>
+          link
+        </Mark>
+        <Mark className="warning" style={buttonStyle}>
+          warning
+        </Mark>
+        <Mark className="danger" style={buttonStyle}>
+          danger
+        </Mark>
+        <Mark className="success" style={buttonStyle}>
+          success
+        </Mark>
 
-      <div style={titleStyle}>outline模式</div>
-      <Mark className="default outline" style={buttonStyle}>
-        default outline
-      </Mark>
-      <Mark className="primary outline" style={buttonStyle}>
-        primary outline
-      </Mark>
-      <Mark className="link outline" style={buttonStyle}>
-        link outline
-      </Mark>
-      <Mark className="warning outline" style={buttonStyle}>
-        warning outline
-      </Mark>
-      <Mark className="danger outline" style={buttonStyle}>
-        danger outline
-      </Mark>
-      <Mark className="success outline" style={buttonStyle}>
-        success outline
-      </Mark>
+        <Divider>Outline</Divider>
+        <Mark className="default outline" style={buttonStyle}>
+          default outline
+        </Mark>
+        <Mark className="primary outline" style={buttonStyle}>
+          primary outline
+        </Mark>
+        <Mark className="link outline" style={buttonStyle}>
+          link outline
+        </Mark>
+        <Mark className="warning outline" style={buttonStyle}>
+          warning outline
+        </Mark>
+        <Mark className="danger outline" style={buttonStyle}>
+          danger outline
+        </Mark>
+        <Mark className="success outline" style={buttonStyle}>
+          success outline
+        </Mark>
 
-      <div style={titleStyle}>light-outline模式</div>
-      <Mark className="default light-outline" style={buttonStyle}>
-        default light-outline
-      </Mark>
-      <Mark className="primary light-outline" style={buttonStyle}>
-        primary light-outline
-      </Mark>
-      <Mark className="link light-outline" style={buttonStyle}>
-        link light-outline
-      </Mark>
-      <Mark className="warning light-outline" style={buttonStyle}>
-        warning light-outline
-      </Mark>
-      <Mark className="danger light-outline" style={buttonStyle}>
-        danger light-outline
-      </Mark>
-      <Mark className="success light-outline" style={buttonStyle}>
-        success light-outline
-      </Mark>
-    </>
+        <Divider>light-outline</Divider>
+        <Mark className="default light-outline" style={buttonStyle}>
+          default light-outline
+        </Mark>
+        <Mark className="primary light-outline" style={buttonStyle}>
+          primary light-outline
+        </Mark>
+        <Mark className="link light-outline" style={buttonStyle}>
+          link light-outline
+        </Mark>
+        <Mark className="warning light-outline" style={buttonStyle}>
+          warning light-outline
+        </Mark>
+        <Mark className="danger light-outline" style={buttonStyle}>
+          danger light-outline
+        </Mark>
+        <Mark className="success light-outline" style={buttonStyle}>
+          success light-outline
+        </Mark>
+      </Layout.Main>
+    </Layout>
   )
 }
