973f7895a4f299d2f6939718e8300b88c52a1495
wangmingzhu <wangmingzhu@qince.com>
2025-02-11 20:47:22 +0800

    fix(5.9.26): fixed SafeArea onResize issues

--- src/components/SafeArea/utils/onResize.js
+++ src/components/SafeArea/utils/onResize.js
@@ -1,12 +1,31 @@
 function onResize(fn) {
   if (!window.autoSafeAreaResizeEvent) {
-    // Listener viewer change
-    requestAnimationFrame(() => {
-      fn()
-    })
+    // Singleton
+    window.autoSafeAreaResizeEvent = true
 
-    // Listener url change
-    // window.addEventListener('popstate', fn, false)
+    // Singleton
+    window.removeEventListener('popstate', fn, false)
+
+    // Press physical back keying, history.back
+    window.addEventListener('popstate', fn, false)
+
+    // 闭包, 防止window.history.pushState被外部修改
+    ;(function () {
+      let originalPushState = window.history.pushState
+
+      // history.push
+      window.history.pushState = function () {
+        fn && fn()
+        originalPushState.apply(window.history, arguments)
+      }
+
+      // Page init, history.replace
+      let originalReplaceState = window.history.replaceState
+      window.history.replaceState = function () {
+        fn && fn()
+        originalReplaceState.apply(window.history, arguments)
+      }
+    })()
   }
 }
 
