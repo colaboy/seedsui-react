02cd9d4705489eb66b367af236c8459274145606
wangmingzhu <wangmingzhu@qince.com>
2025-02-12 09:05:39 +0800

    fix(5.9.27): simpify onReize

--- src/components/SafeArea/utils/onResize.js
+++ src/components/SafeArea/utils/onResize.js
@@ -1,31 +1,29 @@
+// Singleton reize event
 function onResize(fn) {
+  if (typeof fn !== 'function') return
+
+  // Singleton
   if (!window.autoSafeAreaResizeEvent) {
-    // Singleton
     window.autoSafeAreaResizeEvent = true
 
-    // Singleton
-    window.removeEventListener('popstate', fn, false)
-
     // Press physical back keying, history.back
+    window.removeEventListener('popstate', fn, false)
     window.addEventListener('popstate', fn, false)
 
-    // 闭包, 防止window.history.pushState被外部修改
-    ;(function () {
-      let originalPushState = window.history.pushState
+    // 改写原型, 若不单例, 改写两次相同的方法将会死循环
+    // history.push
+    let originalPushState = window.history.pushState
+    window.history.pushState = function () {
+      fn && fn()
+      originalPushState.apply(window.history, arguments)
+    }
 
-      // history.push
-      window.history.pushState = function () {
-        fn && fn()
-        originalPushState.apply(window.history, arguments)
-      }
-
-      // Page init, history.replace
-      let originalReplaceState = window.history.replaceState
-      window.history.replaceState = function () {
-        fn && fn()
-        originalReplaceState.apply(window.history, arguments)
-      }
-    })()
+    // Page init, history.replace
+    let originalReplaceState = window.history.replaceState
+    window.history.replaceState = function () {
+      fn && fn()
+      originalReplaceState.apply(window.history, arguments)
+    }
   }
 }
 
