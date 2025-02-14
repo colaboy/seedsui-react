7322f6e470c6ea65891de4f60396532e99983749
wangmingzhu <wangmingzhu@qince.com>
2025-02-14 10:14:55 +0800

    fix: fixed SafeArea onResize run first function cause closure memo issues

--- src/components/SafeArea/utils/onResize.js
+++ src/components/SafeArea/utils/onResize.js
@@ -1,27 +1,35 @@
+// popsate run latest window.autoSafeAreaResizeHandle
+function handleAutoSafeAreaPopState() {
+  window.autoSafeAreaResizeHandle()
+}
+
 // Singleton reize event
 function onResize(fn) {
   if (typeof fn !== 'function') return
+
+  // Update fn to window.autoSafeAreaResizeHandle
+  window.autoSafeAreaResizeHandle = fn
 
   // Singleton
   if (!window.autoSafeAreaResizeEvent) {
     window.autoSafeAreaResizeEvent = true
 
     // Press physical back keying, history.back
-    window.removeEventListener('popstate', fn, false)
-    window.addEventListener('popstate', fn, false)
+    window.removeEventListener('popstate', handleAutoSafeAreaPopState, false)
+    window.addEventListener('popstate', handleAutoSafeAreaPopState, false)
 
     // 改写原型, 若不单例, 改写两次相同的方法将会死循环
     // history.push
     let originalPushState = window.history.pushState
     window.history.pushState = function () {
-      fn && fn()
+      handleAutoSafeAreaPopState()
       originalPushState.apply(window.history, arguments)
     }
 
     // Page init, history.replace
     let originalReplaceState = window.history.replaceState
     window.history.replaceState = function () {
-      fn && fn()
+      handleAutoSafeAreaPopState()
       originalReplaceState.apply(window.history, arguments)
     }
   }
