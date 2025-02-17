030dc1c09ca7019c366a279e1e5e16c5032429a0
wangmingzhu <wangmingzhu@qince.com>
2025-02-17 19:28:56 +0800

    feat: develop new ToolBar

--- src/components/Space/Compact/index.js
+++ src/components/Space/Compact/index.js
@@ -1,4 +1,4 @@
-import React, { useImperativeHandle, forwardRef, useRef } from 'react'
+import React, { useEffect, useImperativeHandle, forwardRef, useRef } from 'react'
 
 const Compact = forwardRef(({ children, ...props }, ref) => {
   const rootRef = useRef(null)
@@ -10,6 +10,34 @@ const Compact = forwardRef(({ children, ...props }, ref) => {
       getRootDOM: () => rootRef.current
     }
   })
+
+  // Add className to children
+  useEffect(() => {
+    let childrenDOM = rootRef.current.children
+    if (!childrenDOM) return
+
+    // Different component, different child className
+    let componentClassNames = ['button']
+
+    // Each child add compact className
+    for (let index = 0; index < childrenDOM.length; index++) {
+      let child = childrenDOM[index]
+
+      // Each child add different compact className
+      for (let className of componentClassNames) {
+        if (child.classList.contains(className)) {
+          child.classList.add(`${className}-compact-item`)
+          if (index === 0) {
+            child.classList.add(`${className}-compact-first-item`)
+          } else if (index === childrenDOM.length - 1) {
+            child.classList.add(`${className}-compact-last-item`)
+          }
+          break
+        }
+      }
+    }
+    // eslint-disable-next-line
+  }, [])
 
   return (
     <div
