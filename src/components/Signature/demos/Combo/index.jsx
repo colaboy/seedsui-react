3a4414d247735cb23bd9704872954d575da82f1c
wangmingzhu <wangmingzhu@qince.com>
2025-02-21 16:55:30 +0800

    fix: fixed SignatureModal issues

--- src/components/Signature/demos/Combo/index.jsx
+++ src/components/Signature/demos/Combo/index.jsx
@@ -1,6 +1,10 @@
 import React, { useRef, useState } from 'react'
 import { Signature, Layout, Toast } from 'seedsui-react'
 
+// Test safe area
+// import { SafeArea } from 'seedsui-react'
+// SafeArea.autoSafeArea({ debug: true })
+
 export default () => {
   const [value, setValue] = useState(
     'https://image-test.waiqin365.com/6069734652819592543/blog/201912/819415708498937580.png?x-oss-process=style/zk320'
@@ -14,6 +18,9 @@ export default () => {
           backgroundColor="white"
           // disabled={true}
           value={value}
+          modalProps={{
+            safeArea: 'auto'
+          }}
           onChange={(newVal) => {
             console.log(newVal)
             setValue(newVal)
