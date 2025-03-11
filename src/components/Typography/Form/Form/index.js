cec6942438ca58be6e1390f8be8fe1ee883ca997
wangmingzhu <wangmingzhu@qince.com>
2025-03-11 19:43:30 +0800

    feat: develop Form

---
+++ src/components/Typography/Form/Form/index.js
@@ -0,0 +1,49 @@
+import React, { useImperativeHandle, forwardRef, useRef } from 'react'
+import FormContext from './FormContext'
+import Item from './Item'
+import Label from './Label'
+import Main from './Main'
+
+// layout: horizontal | vertical | inline
+const Form = forwardRef(
+  (
+    {
+      virtual,
+      layout = 'horizontal',
+      labelCol,
+      mainCol,
+      scrollerDOM,
+      children,
+      className,
+      ...props
+    },
+    ref
+  ) => {
+    const rootRef = useRef(null)
+
+    // Expose
+    useImperativeHandle(ref, () => {
+      return {
+        rootDOM: rootRef.current,
+        getRootDOM: () => rootRef.current
+      }
+    })
+
+    return (
+      <FormContext.Provider value={{ layout, labelCol, mainCol, scrollerDOM: scrollerDOM }}>
+        <div
+          ref={rootRef}
+          className={`form-items form-layout-${layout}${className ? ' ' + className : ''}`}
+          {...props}
+        >
+          {children}
+        </div>
+      </FormContext.Provider>
+    )
+  }
+)
+
+Form.Item = Item
+Form.Label = Label
+Form.Main = Main
+export default Form
--- src/components/Typography/Form/index.js
+++ src/components/Typography/Form/index.js
@@ -1,38 +1,7 @@
-import React, { useImperativeHandle, forwardRef, useRef } from 'react'
-import FormContext from './FormContext'
+import Form from './Form'
 import Item from './Item'
 import Label from './Label'
 import Main from './Main'
-
-// layout: horizontal | vertical | inline
-const Form = forwardRef(
-  (
-    { layout = 'horizontal', labelCol, mainCol, scrollerDOM, children, className, ...props },
-    ref
-  ) => {
-    const rootRef = useRef(null)
-
-    // Expose
-    useImperativeHandle(ref, () => {
-      return {
-        rootDOM: rootRef.current,
-        getRootDOM: () => rootRef.current
-      }
-    })
-
-    return (
-      <FormContext.Provider value={{ layout, labelCol, mainCol, scrollerDOM: scrollerDOM }}>
-        <div
-          ref={rootRef}
-          className={`form-items form-layout-${layout}${className ? ' ' + className : ''}`}
-          {...props}
-        >
-          {children}
-        </div>
-      </FormContext.Provider>
-    )
-  }
-)
 
 Form.Item = Item
 Form.Label = Label
