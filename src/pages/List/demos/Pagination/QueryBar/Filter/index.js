0556bdbf6edec84c687078807e4b78b86b1d59d0
wangmingzhu <wangmingzhu@qince.com>
2025-03-13 20:43:28 +0800

    feat: develop Pagination

---
+++ src/pages/List/demos/Pagination/QueryBar/Filter/index.js
@@ -0,0 +1,62 @@
+// 第三方库导入
+import React, { useRef, useEffect, useState } from 'react'
+import { Form, Modal, LocaleUtil, Input } from 'seedsui-react'
+// 项目内部模块导入
+// 样式图片等资源文件导入
+
+const locale = LocaleUtil.locale
+
+function Filter({ queryParams, onChange }) {
+  const [form] = Form.useForm()
+  const modifiedRef = useRef(false)
+
+  const [active, setActive] = useState(false)
+  const [visible, setVisible] = useState(false)
+
+  // 显示弹窗时需要还原值
+  useEffect(() => {
+    if (!visible) return
+    form.setFieldsValue(queryParams)
+  }, [visible])
+
+  return (
+    <Modal.FilterCombo
+      className={active ? 'active' : ''}
+      onVisibleChange={(visible) => {
+        setVisible(visible)
+      }}
+      // 取消还原激活状态
+      onCancel={() => {
+        modifiedRef.current = active
+      }}
+      onConfig={() => {
+        console.log('setting')
+      }}
+      onReset={() => {
+        modifiedRef.current = false
+        form.resetFields()
+      }}
+      onOk={({ close }) => {
+        setActive(modifiedRef.current)
+        console.log(form.getFieldsValue())
+        onChange && onChange({ ...queryParams, ...form.getFieldsValue() })
+        close()
+      }}
+    >
+      <Form
+        layout="vertical"
+        form={form}
+        onValuesChange={() => {
+          modifiedRef.current = true
+        }}
+        style={{ marginLeft: '12px' }}
+      >
+        <Form.Item name="input" label={locale('单行文本框')}>
+          <Input.Text placeholder={locale('请输入')} maxLength={50} />
+        </Form.Item>
+      </Form>
+    </Modal.FilterCombo>
+  )
+}
+
+export default Filter
---
+++ src/pages/List/demos/Pagination/QueryBar/index.js
@@ -0,0 +1,39 @@
+// 第三方库导入
+import React, { useState } from 'react'
+import { LocaleUtil, Layout, ToolBar } from 'seedsui-react'
+// 项目内部模块导入
+import Filter from './Filter'
+// 样式图片等资源文件导入
+
+const locale = LocaleUtil.locale
+
+// 筛选栏
+const QueryBar = ({ queryParams, onChange }) => {
+  console.log(queryParams)
+  let [query, setQuery] = useState(queryParams)
+
+  return (
+    <Layout.Header>
+      <ToolBar className="search">
+        {/* 搜索 */}
+        <ToolBar.Search
+          placeholder={locale('按名称/拼音/拼音首字母查询')}
+          value={query?.keyword || ''}
+          onChange={(value) => {
+            setQuery({
+              ...query,
+              keyword: value
+            })
+          }}
+          onSearch={() => {
+            onChange && onChange({ ...query })
+          }}
+        />
+        {/* 筛选弹窗 */}
+        <Filter queryParams={queryParams} onChange={onChange} />
+      </ToolBar>
+    </Layout.Header>
+  )
+}
+
+export default QueryBar
