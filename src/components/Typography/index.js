3a61282869ec8d525720d55d933dda86c990b678
wangmingzhu <wangmingzhu@qince.com>
2025-03-17 10:01:08 +0800

    feat: develop Typography.Text

--- src/components/Typography/index.js
+++ src/components/Typography/index.js
@@ -1,8 +1,10 @@
 import getDisplayValue from './getDisplayValue'
 import Typography from './Typography'
 import Form from './Form'
+import Text from './Letter/Text'
 
 Typography.getDisplayValue = getDisplayValue
 Typography.Form = Form
+Typography.Text = Text
 
 export default Typography
