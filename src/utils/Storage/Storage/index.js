d91de0c9119f096d4a1ef8291f99c38a30cdbcdf
王明柱 <wangmingzhu@wangmingzhus-MacBook-Pro.local>
2025-03-22 15:47:18 +0800

    feat: Storage

--- src/utils/Storage/Storage/index.js
+++ src/utils/Storage/Storage/index.js
@@ -2,14 +2,6 @@ import localforage from 'localforage'
 import LocalStorage from './LocalStorage'
 import SessionStorage from './SessionStorage'
 
-// 内库使用-start
-import Device from './../../Device'
-// 内库使用-end
-
-/* 测试使用-start
-import {  Device } from 'seedsui-react'
-测试使用-end */
-
 // 文档: https://localforage.docschina.org/#api
 const Storage = {
   ...LocalStorage,
@@ -21,30 +13,6 @@ const Storage = {
       successCallback && successCallback(null, null)
       return null
     }
-
-    if (Device.platform === 'wq' && Device.compareVersion(Device.platformVersion, '7.2.20') >= 0) {
-      return new Promise((resolve) => {
-        window.top.wq.getStorage({
-          key: key,
-          success: function (res) {
-            let data = res.data || null
-            try {
-              data = JSON.parse(res.data)
-            } catch (error) {
-              data = res.data || null
-            }
-
-            successCallback && successCallback(null, data)
-            resolve(data)
-          },
-          fail: (err) => {
-            console.error('Storage.getItem:', err)
-            successCallback && successCallback(err, null)
-            resolve(null)
-          }
-        })
-      })
-    }
     return localforage.getItem(key, successCallback)
   },
   // 将数据保存到离线仓库。
@@ -53,24 +21,6 @@ const Storage = {
       console.error('Storage.setItem: key is empty')
       successCallback && successCallback(null, null)
       return null
-    }
-
-    if (Device.platform === 'wq' && Device.compareVersion(Device.platformVersion, '7.2.20') >= 0) {
-      return new Promise((resolve) => {
-        window.top.wq.setStorage({
-          key: key,
-          data: JSON.stringify(value),
-          success: function () {
-            successCallback && successCallback(null, value)
-            resolve(value)
-          },
-          fail: (err) => {
-            console.error('Storage.setItem:', err)
-            successCallback && successCallback(err, value)
-            resolve(null)
-          }
-        })
-      })
     }
     return localforage.setItem(key, value, successCallback)
   },
@@ -81,42 +31,10 @@ const Storage = {
       successCallback && successCallback(null, undefined)
       return undefined
     }
-
-    if (Device.platform === 'wq' && Device.compareVersion(Device.platformVersion, '7.2.20') >= 0) {
-      return new Promise((resolve) => {
-        window.top.wq.removeStorage({
-          key: key,
-          success: function () {
-            successCallback && successCallback(null, undefined)
-            resolve(undefined)
-          },
-          fail: (err) => {
-            console.error('Storage.removeItem:', err)
-            successCallback && successCallback(err, undefined)
-            resolve(undefined)
-          }
-        })
-      })
-    }
     return localforage.removeItem(key, successCallback)
   },
   // 从数据库中删除所有的 key，重置数据库。
   clear: (successCallback) => {
-    if (Device.platform === 'wq' && Device.compareVersion(Device.platformVersion, '7.2.20') >= 0) {
-      return new Promise((resolve) => {
-        window.top.wq.clearStorage({
-          success: function () {
-            successCallback && successCallback(null, undefined)
-            resolve(undefined)
-          },
-          fail: (err) => {
-            console.error('Storage.clear:', err)
-            successCallback && successCallback(err, undefined)
-            resolve(undefined)
-          }
-        })
-      })
-    }
     return localforage.clear(successCallback)
   }
 }
